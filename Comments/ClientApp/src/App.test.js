b83b94d830f79ba1eb4054190d933f0862d261b1
Warren Keith <warren.keith@nice.org.uk>
2018-02-23 15:02:00 +0000

    add additional describe for test FE test

--- Comments/ClientApp/src/App.test.js
+++ Comments/ClientApp/src/App.test.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('App', ()=>{
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-  });
+describe('Client Unit Tests', ()=>{
+  describe('App', ()=>{
+    it('Renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+    });
+    it('Really does render without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+    });
+  })  
 })
