b6b857e0f68536348ff2d433a72ee723fe8dad5b
Warren Keith <warren.keith@nice.org.uk>
2018-02-23 14:52:07 +0000

    add describe for test FE test

--- Comments/ClientApp/src/App.test.js
+++ Comments/ClientApp/src/App.test.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-});
+describe('App', ()=>{
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+  });
+})
