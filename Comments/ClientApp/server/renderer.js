b3e7bb7ee6fdd5d33595e6e62fa267c312026dcb
ediblecode <ian.routledge@nice.org.uk>
2018-02-23 16:18:40 +0000

    Update server rendering to use built in originalHtml property
    Previously we were loading in HTML manually. Now we don't need to do this
    so are using `params.data.originalHtml`. Also includes a refactor of the
    rendering logic and replacement

--- Comments/ClientApp/server/renderer.js
+++ Comments/ClientApp/server/renderer.js
@@ -11,65 +11,69 @@ import * as path from "path";
 
 import App from "./../src/App";
 
-const DevScriptUrl = "/static/js/bundle.js",
-    DevLayoutFolder = "public",
-    ProdLayoutFolder = "build",
-    IsProduction = process.env.NODE_ENV === "production";
+const OpeningHtmlTagRegex: RegExp = /<html(.*?[^?])?>/,
+    OpeningBodyTagRegex: RegExp = /<body(.*?[^?])?>/,
+    ClassAttributeRegex: RegExp = /class=\"([^"]*)\"/,
+    IsProduction: boolean = process.env.NODE_ENV === "production";
 
-// Returns a promise that resolves with the HTML layout file contents.
-// In production, this is the HTML file generated into the *build* folder
-// from create-react-app. In development mode this is the src HTML file
-// from the *public* folder.
-// This HTML file will need some processing e.g. adding script includes etc
-function loadHtmlLayout() {
-    let layoutFolder = IsProduction ? ProdLayoutFolder : DevLayoutFolder,
-        fullLayoutFilePath = path.join(__dirname, "../", layoutFolder, "index.html");
+// Replaces the html tag with the given html attributes
+const replaceHtmlTag = (html: string, htmlAttributes): string => {
+    // Always add a no-js class to support Modernizr
+    let className: string = htmlAttributes.toComponent().className || "",
+        attrs: string = htmlAttributes.toString().replace(ClassAttributeRegex, "");
 
-    return new Promise((resolve, reject) => {
-        fs.readFile(fullLayoutFilePath, "utf8", (err, data) => {
-            if (err)
-                reject(err);
-            resolve(data);
-        });
-    })
-}
-
-// Replace placeholders and tokens in the static html layout file.
-const prepHtml = (data, helmet, body) => {
-    var fullDoc = data
-        .replace(/%PUBLIC_URL%/g, "")
-        .replace("<html lang=\"en\">", `<html ${helmet.htmlAttributes}`)
-        .replace(/<title>(.*?)<\/title>/, helmet.title)
-        .replace("</head>", `${helmet.meta}\r\n${helmet.link}</head>`)
-        .replace("<div id=\"root\"></div>", `<div id="root">${body}</div>`);
-
-    if (!IsProduction) {
-        // In dev mode we run off react dev server so JS is served from that.
-        // In production, the hashed URL is already in the build HTML file.
-        fullDoc = fullDoc.replace("</body>", `<script type="text/javascript" src="${DevScriptUrl}"></script>\r\n</body>`);
-    }
-
-    return fullDoc;
+    return html.replace(OpeningHtmlTagRegex, `<html class="no-js ${className}" ${attrs}>`);
 };
 
+// Replaces the react root div with the given content html
+const replaceBodyTag = (html: string, bodyAttributes): string => {
+    return html.replace(OpeningBodyTagRegex, `<body ${bodyAttributes}>`);
+};
+
+// Replaces the react root div with the given content html
+const replaceRootContent = (html: string, rootContent: string): string => {
+    return html.replace("<div id=\"root\"></div>", `<div id="root">${rootContent}</div>`);
+};
+
+// Replace placeholders and tokens in the static html layout file.
+const prepHtml = (html: string, helmet): string => {
+    return html
+        .replace("<!-- title -->", helmet.title.toString())
+        .replace("<!-- meta -->", helmet.meta.toString())
+        .replace("<!-- link -->", helmet.link.toString())
+        .replace("<!-- script -->", helmet.script.toString());
+};
+
+// Returns a promise that resolves to an object containing the HTML to be rendered.
+// The params contains properties e.g.
+// { location: { protocol, slashes, auth, host, port, hostname, hash, search, query: {}, pathname, path, href },
+// origin, url, baseUrl, absoluteUrl, domainTasks: { }, data: { originalHtml: "", ... }
+// The `params.data` property contains properties set in `SupplyData` in Startup.cs.
 export default createServerRenderer(params => {
     return new Promise((resolve, reject) => {
-        loadHtmlLayout()
-            .then((htmlData) => {
 
-                let context = {};
+        // Context object that Routes can use to pass properties 'out'. Primarily used for status code.
+        // E.g. 
+        //  <Route render={({ staticContext }) => {
+        //      if (staticContext) staticContext.status = 404;
+        //      return null; }} />
+        let context = {};
 
-                const body = renderToString(
-                    <StaticRouter location={params.url} context={ context }>
-                        <App />
-                    </StaticRouter>
-                );
+        const rootContent = renderToString(
+            <StaticRouter location={params.url} context={context}>
+                <App />
+            </StaticRouter>
+        );
 
-                const helmet = Helmet.renderStatic();
+        const helmet = Helmet.renderStatic();
 
-                const html = prepHtml(htmlData, helmet, body);
+        let html = params.data.originalHtml;
+        html = replaceHtmlTag(html, helmet.htmlAttributes);
+        html = replaceBodyTag(html, helmet.bodyAttributes);
+        html = replaceRootContent(html, rootContent);
+        html = prepHtml(html, helmet);
 
-                resolve({ html: html, statusCode: context.status || 200 });
-            });
+        resolve({ html: html, statusCode: context.status || 200 });
+
     });
 });
