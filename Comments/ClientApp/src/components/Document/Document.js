d656259dd9e9c5f106b04c4b3fedda0fd6ffaa93
Warren Keith <warren.keith@nice.org.uk>
2018-03-02 16:52:37 +0000

    Add document view page

--- Comments/ClientApp/src/components/Document/Document.js
+++ Comments/ClientApp/src/components/Document/Document.js
@@ -2,56 +2,37 @@
 
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
+import Breadcrumbs from "./../Breadcrumbs/Breadcrumbs";
 
-class FetchData extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
+type PropsType = {};
 
+class FetchData extends Component<PropsType> {
 	render() {
 		return (
 			<div>
 				<Helmet>
 					<title>Comment on Document</title>
 				</Helmet>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aperiam atque autem commodi consequuntur
-					debitis dicta dolor dolorum, earum et exercitationem, expedita facilis minima numquam odio praesentium, quo
-					quos ratione rerum. Aspernatur commodi mollitia numquam possimus recusandae. Eum laboriosam reiciendis unde
-					voluptatem! Alias asperiores blanditiis consequatur consequuntur delectus doloribus dolorum error eum eveniet
-					ex explicabo facere harum ipsa iste itaque labore laborum magni maxime minima minus modi molestias mollitia
-					nam neque nobis officia officiis omnis perspiciatis provident quaerat quasi quibusdam, quidem repellat
-					repellendus similique sunt totam? Aspernatur at cupiditate dolor facere iusto modi nostrum quo sunt suscipit
-					tempore tenetur ut, velit voluptatibus. Aliquam architecto assumenda, atque autem beatae blanditiis cumque
-					dolor dolorem dolores enim eos, et facilis fuga hic illum incidunt inventore ipsum labore libero modi nihil
-					non odit placeat quasi quia quisquam rem reprehenderit sequi similique sint sit soluta tempora tempore totam
-					ullam voluptas voluptatem. Animi autem ea explicabo itaque quo ratione, rem sit. Aspernatur aut dignissimos
-					dolorem dolores enim ex excepturi fugit id, illum in incidunt ipsum labore laborum, nemo nesciunt nihil nisi
-					nostrum officia perspiciatis sed suscipit unde, vero! Aperiam, iusto provident quam qui quisquam quod
-					reprehenderit sequi? Ab dignissimos dolorem ea eaque error eum ex facilis fuga fugit incidunt itaque iure
-					laboriosam libero maxime mollitia nihil nobis nostrum perferendis placeat quae quam reiciendis repudiandae
-					saepe sapiente sint sit, sunt tempora tempore ullam veritatis voluptate voluptates voluptatibus voluptatum! Ad
-					amet aut autem blanditiis consequatur cumque deserunt dignissimos dolorem earum enim facilis fuga id laborum
-					libero modi perspiciatis porro, qui quia quidem ratione rerum ullam voluptates! Distinctio dolorum fugiat
-					fugit illum maxime nam quae unde. Aliquid animi consequatur corporis, earum error impedit magni, officia quis
-					sapiente soluta tempora voluptas voluptatem? Ad aperiam at, atque dicta et hic iste maxime numquam quia quos
-					repudiandae veritatis. Accusamus at culpa, doloribus earum eius facere fuga impedit, modi nihil obcaecati
-					officia officiis pariatur possimus quia quidem quod reprehenderit? A alias autem cupiditate debitis deleniti
-					dignissimos distinctio dolore enim error expedita harum illo illum in inventore laudantium magnam mollitia nam
-					natus necessitatibus nemo nostrum obcaecati, optio possimus quas quibusdam quo repellat saepe tempora tenetur
-					ut veniam veritatis, vero voluptas! Accusamus earum est eveniet fugit molestiae natus nobis, vitae! Alias
-					animi consequuntur culpa dolor ea eos, est explicabo illum in incidunt necessitatibus officia possimus, quo.
-					Assumenda beatae dignissimos dolores error esse eveniet harum hic id inventore ipsam, libero maxime
-					necessitatibus neque nesciunt nisi non nulla obcaecati officia placeat possimus quam quasi quia quisquam
-					reiciendis sunt suscipit voluptatum? Ad animi aperiam, deleniti dolorem dolorum hic ipsum itaque laborum
-					magnam, molestias numquam provident sapiente sunt? Aut eos facere libero voluptatem. Accusamus delectus ex
-					facilis hic nesciunt, quisquam unde voluptates. Autem culpa dignissimos dolores, eligendi facilis modi nobis
-					omnis quam quisquam sapiente. Aperiam dolores, eos exercitationem nulla possimus quia ratione soluta! Aut,
-					doloremque excepturi, in optio perspiciatis praesentium quaerat quia ratione rem, sequi tempore vero! Amet
-					asperiores aspernatur culpa earum, explicabo facilis iure modi neque nesciunt perspiciatis porro quos
-					reiciendis repellat repellendus similique soluta unde voluptatibus.
-				</p>
+				<Breadcrumbs
+					segments={[
+						{
+							label: "Home",
+							url: "/document"
+						},
+						{
+							label: "NICE Guidance",
+							url: "#"
+						},
+						{
+							label: "In Consulation",
+							url: "#"
+						},
+						{
+							label: "Document title",
+							url: "#"
+						}
+					]}
+				/>
 			</div>
 		);
 	}
