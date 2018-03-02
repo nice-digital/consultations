5847293f486097397dae1a205a1bc9a90910b8f7
Warren Keith <warren.keith@nice.org.uk>
2018-03-02 16:52:21 +0000

    Add alpha banner to document view page

--- Comments/ClientApp/src/components/FullPage.js
+++ Comments/ClientApp/src/components/FullPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class Layout extends Component {
 	render() {
@@ -6,11 +6,19 @@ export default class Layout extends Component {
 			<div className="container">
 				<div className="grid">
 					<div data-g="12">
-						{this.props.children}
+						<p className="phase-banner">
+							<span className="phase-banner__tag">
+								<span className="tag tag--impact tag--alpha">Alpha</span>
+							</span>
+							<span className="phase-banner__label">
+								NICE Consulations is in development. This means it is not feature complete and there may be issues. Find
+								any? <a href="https://github.com/nhsevidence/consultations" rel="noopener noreferrer" target="_blank">Please, let us know!</a>
+							</span>
+						</p>
 					</div>
+					<div data-g="12">{this.props.children}</div>
 				</div>
 			</div>
 		);
 	}
 }
-
