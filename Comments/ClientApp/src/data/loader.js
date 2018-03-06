31163cba5c8d2dffe2be12c4ea9d7a6344ed7d44
Warren Keith <warren.keith@nice.org.uk>
2018-03-06 16:41:25 +0000

    Replace fetch with axios in the loader

--- Comments/ClientApp/src/data/loader.js
+++ Comments/ClientApp/src/data/loader.js
@@ -1,8 +1,9 @@
 import Endpoints from "./endpoints";
+import axios from "axios";
 
 const load = (endpoint) => {
-	return fetch("http://localhost:52679/" + Endpoints[endpoint])
-		.then(response => response.json());
+	return axios("http://localhost:52679/" + Endpoints[endpoint])
+		.then(response => response.data);
 };
 
 export default load;
