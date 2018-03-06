6c7f422bca19db9d24c688c06b38c175edf9c0e7
Warren Keith <warren.keith@nice.org.uk>
2018-03-06 16:26:59 +0000

    Reformat server and data folders for eslint

--- Comments/ClientApp/src/data/loader.js
+++ Comments/ClientApp/src/data/loader.js
@@ -1,6 +1,6 @@
 import Endpoints from "./endpoints";
 
-export const load = (endpoint) => {
+const load = (endpoint) => {
 	return fetch("http://localhost:52679/" + Endpoints[endpoint])
 		.then(response => response.json());
 };
