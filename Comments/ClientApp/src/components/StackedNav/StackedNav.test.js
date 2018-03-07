10d9e4991ca6c2cebc68b9f7776a136c2b390239
Warren Keith <warren.keith@nice.org.uk>
2018-03-07 19:29:38 +0000

    Started StackedNav test

---
+++ Comments/ClientApp/src/components/StackedNav/StackedNav.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import StackedNav from "./StackedNav";
+
+describe("[ClientApp] ", () => {
+	describe("StackedNav ", () => {
+		it("Mounts as a react component", () => {
+			// const wrapper = shallow(<StackedNav />);
+			// const el = wrapper.find("nav");
+			// expect(el).toExist();
+		});
+	});
+});
