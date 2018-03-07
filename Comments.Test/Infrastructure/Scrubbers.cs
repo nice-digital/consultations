777d3a0a75dc1ce8e6f9cc9aebf0007f73daac47
John Williams <jcwilliams@gmail.com>
2018-03-07 10:12:24 +0000

    scrubber hash from html

---
+++ Comments.Test/Infrastructure/Scrubbers.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Comments.Test.Infrastructure
+{
+    public static class Scrubbers 
+    {
+        public static string ScrubHashFromJavascriptFileName(string str)
+        {
+            return Regex.Replace(str, "(src=\\\".*.)(.[a-z0-9]{8}.)(js\\\")", "$1.$3"); //unescaped regex is: src=\".*.([a-z0-9]{8}.)js
+        }
+    }
+}
