67883eb3798aed9dcce68f879dd1c6bf64ef9d9d
John Williams <jcwilliams@gmail.com>
2018-03-07 14:29:40 +0000

    adding consultations hub page placeholder

--- Comments/Controllers/Web/ConsultationsController.cs
+++ Comments/Controllers/Web/ConsultationsController.cs
@@ -1,13 +1,13 @@
-﻿//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-//namespace Comments.Controllers.Web
-//{
-//    [Route("consultations")]
-//    public class ConsultationsController : Controller
-//    {
-//        public IActionResult Index()
-//        {
-//            return View("Index");
-//        }
-//    }
-//}
+namespace Comments.Controllers.Web
+{
+    [Route("consultations")]
+    public class ConsultationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
