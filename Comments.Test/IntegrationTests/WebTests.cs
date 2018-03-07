19be3d275c636583a8b0377b3b95db59fec2ed28
John Williams <jcwilliams@gmail.com>
2018-03-07 14:43:13 +0000

    updating tests

--- Comments.Test/IntegrationTests/WebTests.cs
+++ Comments.Test/IntegrationTests/WebTests.cs
@@ -6,21 +6,21 @@ namespace Comments.Test.IntegrationTests
 {
     public class WebTests : TestBase
     {
-        //[Fact]
-        //public async Task GetConsultationHomepage()
-        //{
-        //    // Act
-        //    var response = await _client.GetAsync("/consultations/1/1");
-        //    response.EnsureSuccessStatusCode();
+        [Fact]
+        public async Task Get_Consultations_Homepage()
+        {
+            // Act
+            var response = await _client.GetAsync("/consultations"); //this route is temporary.
+            response.EnsureSuccessStatusCode();
 
-        //    var responseString = await response.Content.ReadAsStringAsync();
+            var responseString = await response.Content.ReadAsStringAsync();
 
-        //    // Assert
-        //    responseString.ShouldMatchApproved();
-        //}
+            // Assert
+            responseString.ShouldMatchApproved();
+        }
 
         [Fact]
-        public async Task GetReactHomepage()
+        public async Task Get_React_Document_Page()
         {
             // Act
             var response = await _client.GetAsync("/consultations/1/1/introduction");
