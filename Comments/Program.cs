878c729507d4e39545265aecdfeac5ea44984b83
Sue Garner <Sue.Garner@nice.org.uk>
2018-04-27 09:09:55 +0100

    EC-479 Fixed GetCurrentUser error when trying to migrate DB

--- Comments/Program.cs
+++ Comments/Program.cs
@@ -13,40 +13,25 @@ namespace Comments
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
 
-            //var host = BuildWebHost(args);
-            //host.Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetService<ConsultationsContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);  //TODO: Logging
+                    throw;
+                }
+            }
 
-            //    try
-            //    {
-            //        var context = services.GetService<ConsultationsContext>();
-            //        if (context.GetService<IRelationalDatabaseCreator>().Exists())
-            //        {
-            //            context.Database.ExecuteSqlCommand(@"
-            //                CREATE TABLE IF NOT EXISTS ""__EFMigrationsHistory"" (
-            //                    ""MigrationId"" TEXT NOT NULL CONSTRAINT ""PK___EFMigrationsHistory"" PRIMARY KEY,
-            //                    ""ProductVersion"" TEXT NOT NULL
-            //                );
-
-            //                INSERT OR IGNORE INTO ""__EFMigrationsHistory"" (""MigrationId"", ""ProductVersion"")
-            //                VALUES ('20180420104648_InitialMigration', '2.0.2');        
-            //            ");
-            //        }
-            //        context.Database.Migrate();
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.WriteLine(e);  //TODO: Logging
-            //        throw;
-            //    }
-            //}
-
-            //host.Run();
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
