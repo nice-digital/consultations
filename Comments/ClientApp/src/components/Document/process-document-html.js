745e6a1c345d534ffbf566f2231b2c90757a41ea
Warren Keith <warren.keith@nice.org.uk>
2018-05-02 14:14:15 +0100

    EC-458 Add chapter comment button

--- Comments/ClientApp/src/components/Document/process-document-html.js
+++ Comments/ClientApp/src/components/Document/process-document-html.js
@@ -1,62 +1,101 @@
 import React, { Fragment } from "react";
 import ReactHtmlParser, { convertNodeToElement } from "react-html-parser";
 
-// const SectionCommentButton = () => {
-// 	return <button>Comment on this section</button>;
-// };
-
 // onNewCommentClick passed through from <Document />
 export const processDocumentHtml = (
 	incomingHtml,
 	onNewCommentClick,
 	sourceURI
 ) => {
-	function addSectionCommentButtons(node) {
-		// console.log(node);
-		// find the "sections" - anchors with a data-heading-type of "section"
-		// and render them verbatim with a button before them
+	function addButtons(node) {
 
 		if (
 			node.name === "a" &&
 			node.attribs &&
 			node.attribs["data-heading-type"] === "section"
 		) {
-			const isTypeText = child => child.type === "text";
-
-			const childrenThatHaveTypeOfText = node.children.filter(isTypeText);
-
-			const sectionName = childrenThatHaveTypeOfText[0].data;
-
-			const elementId = node.attribs.id;
-
-			return (
-				<Fragment key={0}>
-					<button
-						className="document-comment-container__commentButton"
-						tabIndex={0}
-						onClick={e => {
-							e.preventDefault();
-							onNewCommentClick({
-								placeholder: `Comment on ${sectionName}`,
-								sourceURI: sourceURI,
-								commentText: "",
-								commentOn: "Section",
-								htmlElementID: elementId
-							});
-						}}
-					>
-						<span className="icon icon--comment" aria-hidden="true" />
-						<span className="visually-hidden">
-							Comment on section: {sectionName}
-						</span>
-					</button>
-					{convertNodeToElement(node)}
-				</Fragment>
-			);
+			return addSectionButton(node);
+		} else if (
+			node.name === "a" &&
+			node.attribs &&
+			node.attribs["data-heading-type"] === "chapter"
+		) {
+			return addChapterButton(node);
 		}
+
+	}
+
+	function addSectionButton(node) {
+		const isTypeText = child => child.type === "text";
+
+		const childrenThatHaveTypeOfText = node.children.filter(isTypeText);
+
+		const sectionName = childrenThatHaveTypeOfText[0].data;
+
+		const elementId = node.attribs.id;
+
+		return (
+			<Fragment key={0}>
+				<button
+					className="document-comment-container__commentButton"
+					tabIndex={0}
+					onClick={e => {
+						e.preventDefault();
+						onNewCommentClick({
+							placeholder: `Comment on ${sectionName}`,
+							sourceURI: sourceURI,
+							commentText: "",
+							commentOn: "Section",
+							htmlElementID: elementId
+						});
+					}}
+				>
+					<span className="icon icon--comment" aria-hidden="true" />
+					<span className="visually-hidden">
+						Comment on section: {sectionName}
+					</span>
+				</button>
+				{convertNodeToElement(node)}
+			</Fragment>
+		);
+	}
+
+	function addChapterButton(node) {
+		const isTypeText = child => child.type === "text";
+
+		const childrenThatHaveTypeOfText = node.children.filter(isTypeText);
+
+		const chapterName = childrenThatHaveTypeOfText[0].data;
+
+		const elementId = node.attribs.id;
+
+		return (
+			<Fragment key={0}>
+				<button
+					className="document-comment-container__commentButton"
+					tabIndex={0}
+					onClick={e => {
+						e.preventDefault();
+						onNewCommentClick({
+							placeholder: `Comment on chapter: ${chapterName}`,
+							sourceURI: sourceURI,
+							commentText: "",
+							commentOn: "Chapter",
+							htmlElementID: elementId
+						});
+					}}
+				>
+					<span className="icon icon--comment" aria-hidden="true" />
+					<span className="visually-hidden">
+						Comment on chapter: {chapterName}
+					</span>
+				</button>
+				{convertNodeToElement(node)}
+			</Fragment>
+		);
 	}
 
 	return ReactHtmlParser(incomingHtml, {
-		transform: addSectionCommentButtons
+		transform: addButtons
 	});
 };
