de2f6911d43944b6c414f1de0df62629cb2a6abf
Warren Keith <warren.keith@nice.org.uk>
2018-05-02 16:42:59 +0100

    EC-478 Improved comment box styling

--- Comments/ClientApp/src/components/CommentBox/CommentBox.js
+++ Comments/ClientApp/src/components/CommentBox/CommentBox.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import Moment from "react-moment";
 
 type PropsType = {
 	staticContext?: any,
@@ -44,6 +45,7 @@ export class CommentBox extends Component<PropsType, StateType> {
 	static getDerivedStateFromProps(nextProps) {
 		return {
 			ui: {
+				// todo: revisit this - can't presume that an updated comment list means everything's saved
 				unsavedChanges: false
 			},
 			comment: nextProps.comment
@@ -55,16 +57,28 @@ export class CommentBox extends Component<PropsType, StateType> {
 		const { commentText } = this.state.comment;
 		const placeholder = this.state.comment.placeholder
 			? this.state.comment.placeholder
-			: "Enter your comment here";
+			: null;
 		const tabIndex = this.props.drawerOpen ? "0" : "-1";
 		return (
 			<Fragment>
 				<li className="CommentBox">
 					<form onSubmit={e => this.props.saveHandler(e, this.state.comment)}>
+						<div className="grid">
+							<div data-g="6 push:6">
+								Comment on: {this.state.comment.commentOn}
+							</div>
+						</div>
+
+						<div className="grid">
+							<div data-g="6 push:6">
+								<Moment
+									format="D/M/YYYY - h:mma"
+									date={this.state.comment.lastModifiedDate}
+								/>
+							</div>
+						</div>
 						<div className="form__group form__group--textarea mb--0">
-							<p>Comment on: {this.state.comment.commentOn}</p>
-							<p>last modified date: {this.state.comment.lastModifiedDate}</p>
-							<label className="form__label visually-hidden" htmlFor="textarea">
+							<label className="form__label" htmlFor="textarea">
 								{placeholder}
 							</label>
 							<textarea
@@ -77,19 +91,26 @@ export class CommentBox extends Component<PropsType, StateType> {
 								value={commentText}
 							/>
 						</div>
-						{this.state.comment.commentText && this.state.comment.commentText.length > 0 && (
-							<input
-								tabIndex={tabIndex}
-								className="btn ml--0"
-								type="submit"
-								value={this.state.ui.unsavedChanges ? "Save draft" : "Saved"}
-								disabled={!this.state.ui.unsavedChanges}
-								// below is for testing the save button if you can't post to the server
-								// onClick={()=>this.setState({ui:{unsavedChanges: false}})}
-								//
-							/>
-						)}
-						<button tabIndex={tabIndex} className="btn mr--0 right" onClick={e => this.props.deleteHandler(e, this.state.comment.commentId)}>
+						{this.state.comment.commentText &&
+							this.state.comment.commentText.length > 0 && (
+								<input
+									tabIndex={tabIndex}
+									className="btn ml--0"
+									type="submit"
+									value={this.state.ui.unsavedChanges ? "Save draft" : "Saved"}
+									disabled={!this.state.ui.unsavedChanges}
+									// below is for testing the save button if you can't post to the server
+									// onClick={()=>this.setState({ui:{unsavedChanges: false}})}
+									//
+								/>
+							)}
+						<button
+							tabIndex={tabIndex}
+							className="btn mr--0 right"
+							onClick={e =>
+								this.props.deleteHandler(e, this.state.comment.commentId)
+							}
+						>
 							<span className="visually-hidden">Delete this comment</span>
 							<span className="icon icon--trash" aria-hidden="true" />
 						</button>
