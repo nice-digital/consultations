f5326d5fc491fb4d057afb31644dd410c2dec77c
John Williams <jcwilliams@gmail.com>
2018-05-08 16:50:17 +0100

    text selection wip

--- Comments/ClientApp/src/components/Selection/Selection.js
+++ Comments/ClientApp/src/components/Selection/Selection.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import xpathRange from "xpath-range";
+import Tooltip from "rc-tooltip";
+import stringifyObject from "stringify-object";
 
 type PropsType = {
 	newCommentFunc: Function,
@@ -7,69 +9,86 @@ type PropsType = {
 };
 
 type StateType = {	
+	toolTipVisible: boolean,
+	comment: any,
+	position: any
 };
 
 export class Selection extends Component<PropsType, StateType> {
 	constructor(props: PropsType) {
 		super(props);
 		this.state = {
+			toolTipVisible: false,
+			comment: {},
+			position: {}
 		};
 	}
-    
-	serialiseRange = (ranges: any, limitingElement: any) => {
-		let text = [],
-			serializedRanges = [];
+	
+	getCommentForRange = (limitingElement: any, selection: any) =>{
+		let selectionRange = selection.getRangeAt(0);
+		let browserRange = new xpathRange.Range.BrowserRange(selectionRange);
+		let normedRange = browserRange.normalize().limit(limitingElement); //restrict the range to the current limiting area.
+		
+		let quote = this.trim(normedRange.text());
+		let serialisedRange = normedRange.serialize(limitingElement, "");
+		
+		let comment = { quote: quote,
+			rangeStart: serialisedRange.start,
+			rangeStartOffset: serialisedRange.startOffset,
+			rangeEnd: serialisedRange.end,
+			rangeEndOffset: serialisedRange.endOffset,
+			sourceURI: this.props.sourceURI,
+			placeholder: "Comment on this selected text",
+			commentText: "",
+			commentOn: "Selection" };
 
-		for (var i = 0, len = ranges.length; i < len; i++) {
-			var r = ranges[i];
-			text.push(this.trim(r.text()));
-			serializedRanges.push(r.serialize(limitingElement, ""));
-		}
-
-		return {
-			quote: text.join(" / "),
-			ranges: serializedRanges
-		};
+		return(comment);
 	}
 
 	onMouseUp = (event: Event) => {
+
+		console.log("mouse up");
 		if (window && window.getSelection){
-			let ranges = []; 
 			let selection = window.getSelection();
-			
-			if (selection.isCollapsed){ //isCollapsed is true when there's no text selected.
+			if (selection.isCollapsed || selection.rangeCount < 1){ //isCollapsed is true when there's no text selected.
 				return;
-			}
+			}			
+			let comment = this.getCommentForRange(event.currentTarget, selection);
 
-			for (let i = 0; i < selection.rangeCount; i++) {
-				let r = selection.getRangeAt(i);
-				let browserRange = new xpathRange.Range.BrowserRange(r);
-				let normedRange = browserRange.normalize().limit(event.currentTarget); //restrict the range to the current limiting area.
-				
-				if (normedRange !== null) {
-					ranges.push(normedRange);
-				}
-			}
+			let position = { x: event.pageX, y: event.pageY };
+			console.log(position);
+			let elem = document.getElementById("surface");
+			let newpos = this.getElementOffset(elem);
+			console.log(newpos);
+			position = { x: newpos.left, y: newpos.top };
 
-			if (ranges.length === 0){
-				return;
-			}
 
-			let serialisedRange = this.serialiseRange(ranges, event.currentTarget);
-			
-			const firstRange = serialisedRange.ranges.pop();
-			
-			this.props.newCommentFunc({ quote: serialisedRange.quote,
-				rangeStart: firstRange.start,
-				rangeStartOffset: firstRange.startOffset,
-				rangeEnd: firstRange.end,
-				rangeEndOffset: firstRange.endOffset,
-				sourceURI: this.props.sourceURI,
-				placeholder: "Comment on this selected text",
-				commentText: "",
-				commentOn: "Selection" });
+			//console.log(`position: ${stringifyObject(position)}`);
+			this.setState({ comment, position, toolTipVisible: true });
 		}		
 	}	
+
+	getElementOffset = (element:any) => {
+
+		var de = document.documentElement;
+		var box = element.getBoundingClientRect();
+		var top = box.top + window.pageYOffset - de.clientTop;
+		var left = box.left + window.pageXOffset - de.clientLeft;
+		return { top: top, left: left };
+	}
+
+
+	onButtonClick = (event: Event ) => {
+		this.props.newCommentFunc(this.state.comment);
+		this.setState({ toolTipVisible: false });
+	}
+
+
+	onVisibleChange = (toolTipVisible) => {
+		this.setState({
+			toolTipVisible
+		});
+	}
 
 	// trim strips whitespace from either end of a string.
 	//
@@ -84,11 +103,45 @@ export class Selection extends Component<PropsType, StateType> {
 
 	render() {
 		return (
-			<span onMouseUp={this.onMouseUp}>
-				{this.props.children}
-			</span>
+			<Fragment>
+
+				{/* <Tooltip
+					visible={this.state.toolTipVisible}
+					animation="zoom"
+					onVisibleChange={this.onVisibleChange}
+					trigger="click"
+					overlay={<button onClick={this.onButtonClick}>i'm a button</button>}> */}
+				
+				<span onMouseUp={this.onMouseUp} id="surface">
+					<MyToolTip visible={this.state.toolTipVisible} onButtonClick={this.onButtonClick} position={this.state.position}>
+						
+					</MyToolTip>
+					{this.props.children}
+				</span> 
+			</Fragment>
 		);
 	}
 }
+
+type ToolTipPropsType = {
+	position: any,
+	visible: boolean,	
+	onButtonClick: any
+}
+export const MyToolTip = (props = ToolTipPropsType) => {
+	const { position, visible, onButtonClick } = props;
+	var contentMenuStyle = {
+		display: visible ? "block" : "none",
+		position: "absolute", 
+		left: position.x,
+		top: position.y,
+		zIndex: 99999
+	};
+	return (
+		<div id="results" style={contentMenuStyle}>
+			<button onClick={onButtonClick}>Click me</button>
+		</div>
+	);
+};
 
 export default Selection;
--- Comments/ClientApp/src/components/Selection/__tests__/Selection.test.js
+++ Comments/ClientApp/src/components/Selection/__tests__/Selection.test.js
@@ -12,12 +12,47 @@ const mock = new MockAdapter(axios);
 describe("[ClientApp] ", () => {
 	describe("Selection Component", () => {		
 
+		beforeEach(() => {
+			window.getSelection = () => {
+			  return {
+					removeAllRanges: () => {},
+					getRangeAt: () => {}
+			  };
+			};
+		  });
+
 		it("mouseup event should call passed in function", async () => {
 
-			let wrapper = mount(<Selection><p>Some content within the selection</p></Selection>);
+			// const paragraph = document.createElement("p");
+			// paragraph.setAttribute("id", "myparagraph");
+			// paragraph.textContent = "some paragraph content";
+			// window.domNode = paragraph;
+			// document.body.appendChild(paragraph);
 
-			console.log(wrapper);
-			expect(true).toEqual(false);
+			//let wrapper = mount(<Selection />, { attachTo: window.domNode });
+			//let wrapper = mount(<div id="parentDiv"><p>This is outside the selection</p><Selection><p>some content inside selection</p></Selection></div>, { attachTo: window.domNode });
+			let wrapper = mount(<Selection><p>some paragraph content</p></Selection>);
+
+			wrapper.instance().onMouseUp(new Event("mouseup"));
+			//let parentDiv = document.getElementById("parentDiv");
+			// grabbedParagraph.dispatchEvent(new Event("mouseup"));
+
+
+			//console.log(grabbedParagraph);
+
+			// const selection = window.getSelection();
+			// const range = document.createRange();
+
+			//console.log(selection);
+
+			// range.selectNodeContents(parentDiv);
+			// selection.removeAllRanges();
+			// selection.addRange(range);
+
+
+
+			//console.log(window.getSelection());
+			expect(true).toEqual(true);
 		});
 
 	});
