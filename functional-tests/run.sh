68909f5a71d5cebd622246df217cebf131ff967a
ediblecode <ian.routledge@nice.org.uk>
2018-05-08 09:16:29 +0100

    Update baseurl inside docker compose
    When running tests inside docker compose, the tests need to point to a container
    so we override the baseUrl argument with the hostname from the container

--- functional-tests/run.sh
+++ functional-tests/run.sh
@@ -1,7 +1,11 @@
 # Runs functional tests via Docker
 
+# Avoid "Mount denied" errors for Chrome/Firefox containers on Windows
+# See https://github.com/docker/for-win/issues/1829#issuecomment-376328022
+export COMPOSE_CONVERT_WINDOWS_PATHS=1
+
 docker-compose down --remove-orphans && docker-compose rm -vf
 docker-compose build && docker-compose up -d
-docker-compose run tests npm run test:teamcity -- --host hub || exit $?
+docker-compose run tests npm run test:teamcity -- --host hub -b http://comments/consultations/ || exit $?
 docker cp functionaltests_tests_run_1:/tests/errorShots ./errorShots_docker
 docker-compose down
