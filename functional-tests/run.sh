c53e7f951931dcfc2957994c27d7d3af439ad522
ediblecode <ian.routledge@nice.org.uk>
2018-05-08 12:28:35 +0100

    Change port back to 8081

--- functional-tests/run.sh
+++ functional-tests/run.sh
@@ -6,6 +6,8 @@ export COMPOSE_CONVERT_WINDOWS_PATHS=1
 
 docker-compose down --remove-orphans && docker-compose rm -vf
 docker-compose build && docker-compose up -d
-docker-compose run tests npm run test:teamcity -- --host hub -b http://comments/consultations/ || exit $?
+docker-compose run tests npm run test:teamcity -- --host hub -b http://comments:8081/consultations/ || exit $?
 docker cp functionaltests_tests_run_1:/tests/errorShots ./errorShots_docker
+# TODO: Copy serilog logs out of comments container:
+#docker cp functionaltests_comments_run_1:/app/logs ./logs_docker
 docker-compose down
