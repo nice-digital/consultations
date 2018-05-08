97177cac4d3e53613350deaf3b0cdfcf8ee67b78
Warren Keith <warren.keith@nice.org.uk>
2018-05-08 12:03:43 +0100

    EC-497 Comment Box Styling

--- Comments/ClientApp/src/components/CommentBox/CommentBox.js
+++ Comments/ClientApp/src/components/CommentBox/CommentBox.js
@@ -65,53 +65,65 @@ export class CommentBox extends Component<PropsType, StateType> {
 			lastModifiedDate,
 			quote
 		} = this.state.comment;
-		const placeholder = this.state.comment.placeholder
-			? this.state.comment.placeholder
-			: null;
 		const tabIndex = this.props.drawerOpen ? "0" : "-1";
 		return (
 			<Fragment>
 				<li className="CommentBox">
-					<form onSubmit={e => this.props.saveHandler(e, this.state.comment)}>
-						<div>Comment on: {commentOn}</div>
-						<div>Quote: {quote}</div>
-						<div>
-							Last Modified Date:{" "}
-							<Moment format="D/M/YYYY - h:mma" date={lastModifiedDate} />
-						</div>
-						<div className="form__group form__group--textarea mb--0">
-							<label className="form__label" htmlFor={this.props.unique}>Comment</label>
-							<textarea
-								id={this.props.unique}
-								tabIndex={tabIndex}
-								className="form__input form__input--textarea"
-								onChange={this.textareaChangeHandler}
-								placeholder="Enter your comment here"
-								value={commentText}
-							/>
-						</div>
-						{this.state.comment.commentText &&
-							this.state.comment.commentText.length > 0 && (
-								<input
+					<section>
+						<h1 className="CommentBox__title mt--0">
+							Comment on: <span className="text-capitalize">{commentOn}</span>
+							<br />
+							{quote}
+						</h1>
+						{lastModifiedDate ? (
+							<div className="CommentBox__datestamp pv--d font-weight-bold">
+								Last Modified Date:{" "}
+								<Moment format="D/M/YYYY - h:mma" date={lastModifiedDate} />
+							</div>
+						) : null}
+
+						<form onSubmit={e => this.props.saveHandler(e, this.state.comment)}>
+							<div className="form__group form__group--textarea mb--0">
+								<label
+									className="form__label visually-hidden"
+									htmlFor={this.props.unique}
+								>
+									Comment
+								</label>
+								<textarea
+									id={this.props.unique}
 									tabIndex={tabIndex}
-									className="btn ml--0"
-									type="submit"
-									value={this.state.ui.unsavedChanges ? "Save draft" : "Saved"}
-									disabled={!this.state.ui.unsavedChanges}
-									//
+									className="form__input form__input--textarea"
+									onChange={this.textareaChangeHandler}
+									placeholder="Enter your comment here"
+									value={commentText}
 								/>
-							)}
-						<button
-							tabIndex={tabIndex}
-							className="btn mr--0 right"
-							onClick={e =>
-								this.props.deleteHandler(e, this.state.comment.commentId)
-							}
-						>
-							<span className="visually-hidden">Delete this comment</span>
-							<span className="icon icon--trash" aria-hidden="true" />
-						</button>
-					</form>
+							</div>
+							{this.state.comment.commentText &&
+								this.state.comment.commentText.length > 0 && (
+									<input
+										tabIndex={tabIndex}
+										className="btn ml--0"
+										type="submit"
+										value={
+											this.state.ui.unsavedChanges ? "Save draft" : "Saved"
+										}
+										disabled={!this.state.ui.unsavedChanges}
+										//
+									/>
+								)}
+							<button
+								tabIndex={tabIndex}
+								className="btn mr--0 right"
+								onClick={e =>
+									this.props.deleteHandler(e, this.state.comment.commentId)
+								}
+							>
+								<span className="visually-hidden">Delete this comment</span>
+								<span className="icon icon--trash" aria-hidden="true" />
+							</button>
+						</form>
+					</section>
 				</li>
 			</Fragment>
 		);
