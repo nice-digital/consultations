e5b6e84ffd6a3045b7e4b5eb1e5a806aaa00e023
ediblecode <ian.routledge@nice.org.uk>
2018-05-08 15:08:36 +0100

    Error shots now get copied out of the tests docker container

--- functional-tests/run.sh
+++ functional-tests/run.sh
@@ -6,8 +6,13 @@ export COMPOSE_CONVERT_WINDOWS_PATHS=1
 
 docker-compose down --remove-orphans && docker-compose rm -vf
 docker-compose build && docker-compose up -d
-docker-compose run tests npm run test:teamcity -- --host hub -b http://comments:8081/consultations/ || exit $?
+
+docker-compose run tests npm run test:teamcity -- --host hub -b http://comments:8081/consultations/
+exitCode=$?
+
 docker cp functional-tests_tests_run_1:/tests/errorShots ./errorShots_docker
 # TODO: Copy serilog logs out of comments container:
 #docker cp functionaltests_comments_run_1:/app/logs ./logs_docker
 docker-compose down
+
+exit $exitCode
