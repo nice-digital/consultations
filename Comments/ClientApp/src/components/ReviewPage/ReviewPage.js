43dd2e74246c9d03ab395448d399d42589b8500d
Warren Keith <warren.keith@nice.org.uk>
2018-05-22 17:02:24 +0100

    EC-305 Review Breadcrumbs

--- Comments/ClientApp/src/components/ReviewPage/ReviewPage.js
+++ Comments/ClientApp/src/components/ReviewPage/ReviewPage.js
@@ -71,22 +71,21 @@ export class ReviewPage extends Component {
 	};
 
 	getBreadcrumbs = () => {
+		const { consultationId } = this.props.match.params;
+		const consultationsFirstDocument = this.state.documentsList[0].documentId;
+		const firstDocumentChapterSlug = this.state.documentsList[0].chapters[0].slug;
 		return [
 			{
-				label: "Home",
-				url: "/document"
+				label: "All Consultations",
+				url: "https://alpha.nice.org.uk/guidance/inconsultation" // todo: link to current consultation "consultation list page" on dev?
 			},
 			{
-				label: "NICE Guidance",
-				url: "#"
+				label: "Consultation", // todo: consultation overview link to come from API endpoint
+				url: "https://alpha.nice.org.uk/guidance/indevelopment/gid-dg10046/consultation/html-content" // todo: demo - hardcoded to jane's overview
 			},
 			{
-				label: "In Consultation",
-				url: "#"
-			},
-			{
-				label: "Document title",
-				url: "#"
+				label: "Documents",
+				url: `${consultationId}/${consultationsFirstDocument}/${firstDocumentChapterSlug}`
 			}
 		];
 	};
@@ -96,7 +95,7 @@ export class ReviewPage extends Component {
 	};
 
 	render() {
-		if (!this.state.documentsList) return <h1>Loading...</h1>;
+		if (this.state.documentsList.length === 0) return <h1>Loading...</h1>;
 
 		return (
 			<Fragment>
@@ -118,8 +117,7 @@ export class ReviewPage extends Component {
 										onNewCommentClick={this.onNewCommentClick()}
 										url="1/1/Introduction"
 									/>
-									<span>Comments for review</span>
-
+									<span>Review your comments</span>
 									<StickyContainer className="grid">
 										<div data-g="12 md:3">
 											<StackedNav links={this.generateDocumentList(this.state.documentsList)}	/>
