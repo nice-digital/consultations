ca135b60fc20e9d4fb9c7709603ae76742098104
Sue Garner <Sue.Garner@nice.org.uk>
2018-05-29 13:28:39 +0100

    EC-305 Added tests

--- Comments/ClientApp/src/components/ReviewPage/__tests__/ReviewPage.test.js
+++ Comments/ClientApp/src/components/ReviewPage/__tests__/ReviewPage.test.js
@@ -8,6 +8,11 @@ import MockAdapter from "axios-mock-adapter";
 import { generateUrl } from "../../../data/loader";
 import { nextTick, queryStringToObject } from "../../../helpers/utils";
 import ReviewPageWithRouter, {ReviewPage} from "../ReviewPage";
+import ConsultationData from "./Consultation";
+import DocumentsData from "./Documents";
+import ChapterData from "./Chapter";
+import ReviewData from "./ReviewData";
+import toJson from "enzyme-to-json";
 
 const mock = new MockAdapter(axios);
 
@@ -26,9 +31,15 @@ jest.mock("../../../context/UserContext", () => {
 describe("[ClientApp] ", () => {
 	describe("ReviewPage Component", () => {
 		const fakeProps = {
+			match: {
+				url: "/1/review",
+				params: {
+					consultationId: 1,
+				}
+			},
 			location: {
-				pathname: "",
-				search: ""
+				pathname: "/1/review",
+				search: "?sourceURI=consultations%3A%2F%2F.%2Fconsultation%2F1%2Fdocument%2F1"
 			}
 		};
 
@@ -37,7 +48,7 @@ describe("[ClientApp] ", () => {
 		});
 
 		it("generateDocumentList doesn't filter out documents supporting comments", async () => {
-			
+
 			const docTypesIn = [
 				{title : "first doc title", sourceURI: "first source uri", supportsComments: true},
 				{title : "second doc title", sourceURI: "second source uri", supportsComments: true}];
@@ -47,11 +58,11 @@ describe("[ClientApp] ", () => {
 			const returnValue = reviewPage.generateDocumentList(docTypesIn);
 
 			//generateDocumentList unshifts 'All document comments' onto the array so result is docTypesIn + 1
-			expect(returnValue.links.length).toEqual(3);				
+			expect(returnValue.links.length).toEqual(3);
 		});
 
 		it("generateDocumentList filters out documents not supporting comments", async () => {
-			
+
 			const docTypesIn = [
 				{title : "first doc title", sourceURI: "first source uri", supportsComments: true},
 				{title : "second doc title", sourceURI: "second source uri", supportsComments: false}];
@@ -60,7 +71,76 @@ describe("[ClientApp] ", () => {
 
 			const returnValue = reviewPage.generateDocumentList(docTypesIn);
 
-			expect(returnValue.links.length).toEqual(2);				
+			expect(returnValue.links.length).toEqual(2);
 		});
+
+		it("generateDocumentList puts 'All document comments' at front of array", async () => {
+
+			const docTypesIn = [
+				{title : "first doc title", sourceURI: "first source uri", supportsComments: true},
+				{title : "second doc title", sourceURI: "second source uri", supportsComments: false}];
+
+			const reviewPage = new ReviewPage(fakeProps);
+
+			const returnValue = reviewPage.generateDocumentList(docTypesIn);
+
+			expect(returnValue.links[0].label).toEqual("All document comments");
+		});
+
+		it("queryStringToObject should return an object", async () => {
+			const returnValue = queryStringToObject("?search=foo&id=bar");
+			expect(returnValue.search).toEqual("foo");
+			expect(returnValue.id).toEqual("bar");
+		});
+
+		// it("should match snapshot with supplied data", () => {
+
+		// 	const mock = new MockAdapter(axios);
+
+		// 	const wrapper = mount(
+		// 		<MemoryRouter>
+		// 			<ReviewPage {...fakeProps} />
+		// 		</MemoryRouter>
+		// 	);
+
+		// 	mock.onGet("/consultations/api/Review/undefined")
+		// 		.reply(()=>{
+		// 			console.log("hit review mock endpoint");
+		// 			return [200, ReviewData];
+		// 		});
+
+		// 	let documentsPromise = new Promise(resolve => {
+		// 		mock
+		// 			.onGet("/consultations/api/Documents?consultationId=1")
+		// 			.reply(() => {
+		// 				resolve();
+		// 				return [200, DocumentsData];
+		// 			});
+		// 	});
+
+		// 	let consulatationPromise = new Promise(resolve => {
+		// 		mock
+		// 			.onGet("/consultations/api/Consultation?consultationId=1")
+		// 			.reply(() => {
+		// 				resolve();
+		// 				return [200, ConsultationData];
+		// 			});
+		// 	});
+
+		// 	return Promise.all([
+		// 		documentsPromise,
+		// 		consulatationPromise
+		// 	]).then(async () => {
+		// 		await nextTick();
+		// 		wrapper.update();
+		// 		expect(
+		// 			toJson(wrapper, {
+		// 				noKey: true,
+		// 				mode: "deep"
+		// 			})
+		// 		).toMatchSnapshot();
+		// 	});
+		// });
+
 	});
 });
