8d0fea4509eccb15eb0d3d6b23df6b41a607383b
Warren Keith <warren.keith@nice.org.uk>
2018-06-06 16:44:29 +0100

    EC-531 Add footnote links with smooth scroll

--- Comments/ClientApp/src/components/Document/html-transforms/internal-link.js
+++ Comments/ClientApp/src/components/Document/html-transforms/internal-link.js
@@ -1,8 +1,18 @@
-import React, { Fragment } from "react";
-import { convertNodeToElement } from "react-html-parser";
-import { nodeIsTypeText } from "./types";
+import React from "react";
+import { NavHashLink } from "react-router-hash-link";
 
 export default function processInternalLink(node) {
-	// todo: carry on here
-	return convertNodeToElement(node);
+	const props = node.attribs;
+	props.className = props.class;
+	props.to = props.href;
+	props.key = props.href;
+	delete props.class;
+	const label = node.children[0]["data"] || props.href;
+	return (
+		<NavHashLink
+			{...props}
+			scroll={el => el.scrollIntoView({behavior: "smooth", block: "start"})}>
+			{label}
+		</NavHashLink>
+	);
 }
