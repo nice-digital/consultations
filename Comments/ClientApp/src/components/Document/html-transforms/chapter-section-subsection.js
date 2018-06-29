6f05b102b442ae3a0e7b29f807541125a3362383
Warren Keith <warren.keith@nice.org.uk>
2018-06-29 12:00:03 +0100

    EC-525 WIP Refactor document processing for subsection

---
+++ Comments/ClientApp/src/components/Document/html-transforms/chapter-section-subsection.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+import { convertNodeToElement } from "react-html-parser";
+import { nodeIsTypeText, nodeIsSubsection, nodeIsSpanTag } from "./types";
+
+export default function processChapterSection(node, incomingHtml, onNewCommentClick, sourceURI) {
+	let commentOn = node.attribs["data-heading-type"].toLowerCase();
+	let quote = node.children.filter(nodeIsTypeText)[0].data;
+
+	if (nodeIsSubsection(node)) {
+		quote = node.children.filter(nodeIsSpanTag)[0].children.filter(nodeIsTypeText)[0].data;
+		commentOn = "subsection";
+	}
+
+	const htmlElementID = (commentOn === "section" || commentOn === "subsection") ? node.attribs.id : "";
+
+	return (
+		<Fragment key={0}>
+			<button
+				data-qa-sel="in-text-comment-button"
+				className="document-comment-container__commentButton"
+				tabIndex={0}
+				onClick={e => {
+					e.preventDefault();
+					onNewCommentClick({
+						sourceURI,
+						commentText: "",
+						commentOn,
+						htmlElementID,
+						quote
+					});
+				}}
+			>
+				<span className="icon icon--comment" aria-hidden="true" />
+				<span className="visually-hidden">Comment on {commentOn}: {quote}</span>
+			</button>
+			{convertNodeToElement(node)}
+		</Fragment>
+	);
+}
--- Comments/ClientApp/src/components/Document/process-document-html.js
+++ Comments/ClientApp/src/components/Document/process-document-html.js
@@ -1,76 +1,16 @@
 import ReactHtmlParser from "react-html-parser";
-import { nodeIsChapter, nodeIsInternalLink, nodeIsSection } from "./html-transforms/types";
-import processChapterSection from "./html-transforms/chapter-section";
+import { nodeIsChapter, nodeIsInternalLink, nodeIsSection, nodeIsSubsection } from "./html-transforms/types";
+import processChapterSectionSubsection from "./html-transforms/chapter-section-subsection";
 import processInternalLink from "./html-transforms/internal-link";
 
-function nodeIsChapter(node){
-	return node.name === "a" && node.attribs && node.attribs["data-heading-type"] === "chapter";
-}
-
-function nodeIsSection(node) {
-	return node.name === "a" && node.attribs && node.attribs["data-heading-type"] === "section";
-}
-
-function nodeIsSubsection(node) {
-	return node.name === "p" && node.attribs && node.attribs["data-heading-type"] === "numbered-paragraph";
-}
-
-function nodeIsTypeText(node) {
-	return node.type === "text";
-}
-
-function nodeIsSpanTag(node) {
-	return node.name === "span";
-}
-
 // onNewCommentClick passed through from <Document />
-export const processDocumentHtml = (
-	incomingHtml,
-	onNewCommentClick,
-	sourceURI
-) => {
-	function addButtons(node) {
-		const isChapter = nodeIsChapter(node);
-		const isSection = nodeIsSection(node);
-		const isSubsection = nodeIsSubsection(node);
-		if (
-			isChapter || isSection || isSubsection
-		) {
-			let elementType = node.attribs["data-heading-type"].toLowerCase();
-
-			let elementName = node.children.filter(nodeIsTypeText)[0].data;
-
-			if (isSubsection) {
-				elementName = node.children.filter(nodeIsSpanTag)[0].children.filter(nodeIsTypeText)[0].data;
-				elementType = "subsection";
-			}
-
-			const elementId = (elementType === "section" || elementType === "subsection") ? node.attribs.id : "";
-
-			return (
-				<Fragment key={0}>
-					<button
-						className="document-comment-container__commentButton"
-						tabIndex={0}
-						onClick={e => {
-							e.preventDefault();
-							onNewCommentClick({
-								sourceURI: sourceURI,
-								commentText: "",
-								commentOn: elementType,
-								htmlElementID: elementId,
-								quote: elementName
-							});
-						}}
-					>
-						<span className="icon icon--comment" aria-hidden="true" />
-						<span className="visually-hidden">
-							Comment on {elementType}: {elementName}
-						</span>
-					</button>
-					{convertNodeToElement(node)}
-				</Fragment>
-			);
+export const processDocumentHtml = (incomingHtml, onNewCommentClick, sourceURI) => {
+	function transformHtml(node) {
+		if (nodeIsChapter(node) || nodeIsSection(node) || nodeIsSubsection(node)) {
+			return processChapterSectionSubsection(node, incomingHtml, onNewCommentClick, sourceURI);
+		}
+		if (nodeIsInternalLink(node)) {
+			return processInternalLink(node);
 		}
 	}
 
