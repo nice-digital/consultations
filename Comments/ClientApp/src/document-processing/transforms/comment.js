f118d1d0068ba4c33334f6b71eef49f4aa2eed66
Warren Keith <warren.keith@nice.org.uk>
2018-07-23 14:01:39 +0100

    EC-513 Indev Preview with tests

--- Comments/ClientApp/src/document-processing/transforms/comment.js
+++ Comments/ClientApp/src/document-processing/transforms/comment.js
@@ -9,16 +9,16 @@ export default function processComment(node: Object) {
 	const errorInitial = errorStatus.match(/[A-Z]/)[0]; // "E"
 	const errorMessage = errorArray[1].trim(); // "Inline graphic objects aren't supported by the converter"
 
-	const toggleDetail = event => {
-		const relatedDetails = event.target.nextElementSibling;
-		relatedDetails.classList.toggle("visible");
+	const toggleDetail = e => {
+		const relatedDetails = e.target.nextElementSibling;
+		relatedDetails.classList.toggle("active");
 	};
 
 	return (
-		<div tabIndex="0" role="button" className={`ConversionError ConversionError--${errorInitial}`}
+		<div key={node} className={`ConversionError ConversionError--${errorInitial}`}
 			 onClick={(e) => toggleDetail(e)}>
 			<button
-				className={`visible ConversionError__Button ConversionError__Button--${errorInitial}`}>
+				className={`ConversionError__Button ConversionError__Button--${errorInitial}`}>
 				{errorInitial}
 			</button>
 			<ul className={`ConversionError__details ConversionError__details--${errorInitial}`}>
