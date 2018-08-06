11fd1fd030afd6a5bb460f2db0549bc0770d2aac
John Williams <jcwilliams@gmail.com>
2018-08-06 14:12:12 +0100

    EC-579 - removing old review page and fixing comment list

--- Comments/ClientApp/src/components/CommentList/CommentList.js
+++ Comments/ClientApp/src/components/CommentList/CommentList.js
@@ -2,16 +2,18 @@
 
 import React, { Component, Fragment } from "react";
 import { withRouter, Link } from "react-router-dom";
-import { load } from "../../data/loader";
+//import stringifyObject from "stringify-object";
+
 import preload from "../../data/pre-loader";
+import { load } from "../../data/loader";
+import { queryStringToObject, replaceFormat } from "../../helpers/utils";
+import { saveCommentHandler, deleteCommentHandler, saveAnswerHandler, deleteAnswerHandler } from "../../helpers/editing-and-deleting";
+import { pullFocusById } from "../../helpers/accessibility-helpers";
+
 import { CommentBox } from "../CommentBox/CommentBox";
 import { Question } from "../Question/Question";
 import { LoginBanner } from "../LoginBanner/LoginBanner";
 import { UserContext } from "../../context/UserContext";
-import { queryStringToObject, replaceFormat } from "../../helpers/utils";
-import { saveCommentHandler, deleteCommentHandler, saveAnswerHandler, deleteAnswerHandler } from "../../helpers/editing-and-deleting";
-import { pullFocusById } from "../../helpers/accessibility-helpers";
-//import stringifyObject from "stringify-object";
 
 type PropsType = {
 	staticContext?: any,
@@ -56,7 +58,7 @@ export class CommentList extends Component<PropsType, StateType> {
 			preloadedData = this.props.staticContext.preload.data; //this is data from Configure => SupplyData in Startup.cs. the main thing it contains for this call is the cookie for the current user.
 		}
 
-		const preloaded = preload(
+		const preloadedCommentsData = preload(
 			this.props.staticContext,
 			"comments",
 			[],
@@ -64,31 +66,32 @@ export class CommentList extends Component<PropsType, StateType> {
 			preloadedData
 		);
 
-		if (preloaded) {
-			this.setCommentListState(preloaded);
+		if (preloadedCommentsData) {
+			let allowComments = preloadedCommentsData.consultationState.consultationIsOpen && !preloadedCommentsData.consultationState.userHasSubmitted;
+			this.state = {
+				comments: preloadedCommentsData.comments,
+				questions: preloadedCommentsData.questions,
+				loading: false,
+				allowComments: allowComments,
+				error: "",
+				initialDataLoaded: true,
+			};
 		}
 	}
 
 	loadComments() {
 		load("comments", undefined, [], { sourceURI: this.props.match.url }).then(
 			response => {
-				this.setCommentListState(response.data);
+				let allowComments = response.data.consultationState.consultationIsOpen && !response.data.consultationState.userHasSubmitted;
+				this.setState({
+					comments: response.data.comments,
+					questions: response.data.questions,
+					loading: false,
+					allowComments: allowComments,
+				});
 			})
 			.catch(err => console.log("load comments in commentlist " + err));
 	}
-
-	setCommentListState = (commentsData: any) =>
-	{
-		let allowComments = commentsData.consultationState.consultationIsOpen && !commentsData.consultationState.userHasSubmitted;
-		// const comments = this.filterComments(this.props.location.search, commentsData.comments );
-		// const questions = this.filterQuestions(this.props.location.search, commentsData.questions );
-		this.setState({
-			comments: commentsData.comments,
-			questions: commentsData.questions,
-			loading: false,
-			allowComments: allowComments,
-		});
-	};
 
 	componentDidMount() {
 		if (!this.state.initialDataLoaded){
@@ -106,14 +109,6 @@ export class CommentList extends Component<PropsType, StateType> {
 			this.loadComments();
 		}
 	}
-
-	getComments = () => {
-		return this.state.comments;
-	}
-
-	getQuestions = () => {
-		return this.state.questions;
-	}	
 
 	newComment(newComment: CommentType) {
 		let comments = this.state.comments;
@@ -152,8 +147,8 @@ export class CommentList extends Component<PropsType, StateType> {
 	}
 
 	render() {
-		const commentsToShow = this.state.comments.filter(comment => !comment.show);
-		const questionsToShow = this.state.questions.filter(question => !question.show);
+		const commentsToShow = this.state.comments;
+		const questionsToShow = this.state.questions;
 		return (
 			<UserContext.Consumer>
 				{ (contextValue: ContextType) => {
@@ -186,12 +181,12 @@ export class CommentList extends Component<PropsType, StateType> {
 
 									<Fragment>
 										{this.props.viewComments ? (
-											commentsToShow.length === 0 ? <p>No comments yet</p> :
+											this.state.comments.length === 0 ? <p>No comments yet</p> :
 											<ul className="CommentList list--unstyled">
-												{commentsToShow.map((comment) => {
+												{this.state.comments.map((comment) => {
 													return (
 														<CommentBox
-															readOnly={!this.state.allowComments || this.props.isSubmitted}
+															readOnly={!this.state.allowComments}
 															isVisible={this.props.isVisible}
 															key={comment.commentId}
 															unique={`Comment${comment.commentId}`}
@@ -207,10 +202,10 @@ export class CommentList extends Component<PropsType, StateType> {
 												<p>We would like to hear your views on the draft recommendations presented in the guideline, and any comments you may have on the rationale and impact sections in the guideline and the evidence presented in the evidence reviews documents. We would also welcome views on the Equality Impact Assessment.</p>
 												<p>We would like to hear your views on these questions:</p>
 												<ul className="CommentList list--unstyled">
-													{questionsToShow.map((question) => {
+													{this.state.questions.map((question) => {
 														return (
 															<Question
-																readOnly={!this.state.allowComments || this.props.isSubmitted}
+																readOnly={!this.state.allowComments}
 																isVisible={this.props.isVisible}
 																key={question.questionId}
 																unique={`Comment${question.questionId}`}
