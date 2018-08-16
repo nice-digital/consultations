162a4dc66d940af44c3372c0818015ad85c9d2b4
Warren Keith <warren.keith@nice.org.uk>
2018-08-16 15:25:14 +0100

    EC-399 FE tests finished

--- Comments/ClientApp/src/components/SubmitResponseDialog/__tests__/SubmitResponseDialog.test.js
+++ Comments/ClientApp/src/components/SubmitResponseDialog/__tests__/SubmitResponseDialog.test.js
@@ -1,7 +1,7 @@
 /* global jest */
 
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import { SubmitResponseDialog } from "../SubmitResponseDialog";
 
 describe("[ClientApp] ", () => {
@@ -11,33 +11,47 @@ describe("[ClientApp] ", () => {
 			isAuthorised: true,
 			userHasSubmitted: false,
 			validToSubmit: true,
-			submitConsultation: () => {
-				return jest.mock();
-			},
-			inputChangeHandler: () => {
-				return jest.mock();
-			},
-			organisationName: "NICE",
-			tobaccoDisclosure: "Used to work for a tobacco company",
+			submitConsultation: jest.fn(),
+			inputChangeHandler: jest.fn(),
+			organisationName: "",
+			tobaccoDisclosure: "",
 		};
+
+		it("should fire parent change handler if the input values change", () => {
+			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
+			wrapper.find("input#organisationName").simulate("change", {target: {value: "h"}});
+			wrapper.find("input#organisationName").simulate("change", {target: {value: "i"}});
+			wrapper.find("input#organisationName").simulate("change", {target: {value: "!"}});
+			expect(fakeProps.inputChangeHandler.mock.calls[2][0].target.value).toBe("!");
+			expect(fakeProps.inputChangeHandler.mock.calls.length).toEqual(3);
+		});
+
+		it("should fire parent submit function when the submit button is clicked", () => {
+			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
+			wrapper.find("button").simulate("click");
+			expect(fakeProps.submitConsultation.mock.calls.length).toEqual(1);
+		});
+
+		it("should not fire parent submit function when the submit button is clicked, if form is in invalid state", () => {
+			const localProps = fakeProps;
+			localProps.validToSubmit = false;
+			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			const button = wrapper.find("button");
+			expect(button.prop("disabled")).toBe(true);
+		});
+
+		it("should not display a submit button if the current user isn't authorised", () => {
+			const localProps = fakeProps;
+			localProps.isAuthorised = false;
+			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(wrapper.find("button").length).toEqual(0);
+		});
 
 		it("should show thank you message if user has submitted", () => {
 			const localProps = fakeProps;
 			localProps.userHasSubmitted = true;
 			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
 			expect(wrapper.find("p").text()).toEqual("Thank you, your response has been submitted.");
-		});
-
-		it("should fire parent change handler if the input values change", () => {
-			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
-		});
-
-		it("should fire parent submit function when the submit button is clicked", () => {
-			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
-		});
-
-		it("should not render anything if the user isn't authorised", () => {
-			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
 		});
 
 	});
