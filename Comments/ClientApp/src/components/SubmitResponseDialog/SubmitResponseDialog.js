c9aede45789730f49c0c7d3dd3864a4d79372c3e
Warren Keith <warren.keith@nice.org.uk>
2018-08-17 13:16:21 +0100

    EC-399 Radio buttons in

--- Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
+++ Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
@@ -1,75 +1,165 @@
-import React, {Fragment} from "react";
+import React, {Component, Fragment} from "react";
 
-export const SubmitResponseDialog = props => {
-	const {
-		isAuthorised,
-		userHasSubmitted,
-		validToSubmit,
-		submitConsultation,
-		inputChangeHandler,
-		organisationName,
-		tobaccoDisclosure } = props;
+export class SubmitResponseDialog extends Component {
 
-	return (
-		<Fragment>
-			{userHasSubmitted ?
-				<div className="hero">
-					<div className="hero__container">
-						<div className="hero__body">
-							<div className="hero__copy">
-								<p className="hero__intro" data-qa-sel="submitted-text">Thank you, your response has been submitted.</p>
+	constructor(){
+		super();
+		this.state = {
+			showOrganisationInput: false,
+			showTobaccoInput: false,
+		};
+	}
+
+	render(){
+		const {
+			isAuthorised,
+			userHasSubmitted,
+			validToSubmit,
+			submitConsultation,
+			inputChangeHandler,
+			organisationName,
+			tobaccoDisclosure } = this.props;
+		return(
+			<Fragment>
+				{userHasSubmitted ?
+					<div className="hero">
+						<div className="hero__container">
+							<div className="hero__body">
+								<div className="hero__copy">
+									<p className="hero__intro" data-qa-sel="submitted-text">Thank you, your response has been submitted.</p>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				:
-				<div className="hero">
-					<div className="hero__container">
-						<div className="hero__body">
-							<div className="hero__copy">
-								<p className="hero__intro">You are about to submit your final response to NICE</p>
-								<p>After submission you won't be able to:</p>
-								<ul>
-									<li>edit your comments further</li>
-									<li>add any extra comments.</li>
-								</ul>
+					:
+					<div className="hero">
+						<div className="hero__container">
+							<div className="hero__body">
+								<div className="hero__copy">
 
-								<fieldset className="form__fieldset">
-									<legend className="form__legend">Please answer the below questions before submitting</legend>
-									<div className="form__group form__group--text">
-										<label htmlFor="organisationName" className="form__label">
-											Organisation
-										</label>
-										<input id="organisationName" name="organisationName" value={organisationName} className="form__input" type="text" onChange={inputChangeHandler}/>
-										<div className="form__hint form__hint--inverse">If you are commenting on behalf of an organisation, please enter the organisation name</div>
+									<p className="hero__intro">You are about to submit your final response to NICE</p>
+									<p>After submission you won't be able to:</p>
+									<ul>
+										<li>edit your comments further</li>
+										<li>add any extra comments.</li>
+									</ul>
+
+									<p>You must answer the following questions before you submit</p>
+
+									<p>Are you responding on behalf of an organisation?</p>
+
+									<div>
+
+										<div className="form__group form__group--radio form__group--inline">
+											<input
+												className="form__radio"
+												id="respondingAsOrganisation--true"
+												type="radio"
+												name="respondingAsOrganisation"
+												value="true"/>
+											<label
+												className="form__label form__label--radio"
+												htmlFor="respondingAsOrganisation--true">
+												Yes
+											</label>
+										</div>
+
+										<div className="form__group form__group--radio form__group--inline">
+											<input
+												className="form__radio"
+												id="respondingAsOrganisation--false"
+												type="radio"
+												name="respondingAsOrganisation"
+												value="false"/>
+											<label
+												className="form__label form__label--radio"
+												htmlFor="respondingAsOrganisation--false">
+												No
+											</label>
+										</div>
+
 									</div>
-									<div className="form__group form__group--textarea">
-										<label htmlFor="tobaccoDisclosure" className="form__label">
-											Disclose tobacco industry links
-										</label>
-										<textarea id="tobaccoDisclosure" name="tobaccoDisclosure" value={tobaccoDisclosure} className="form__input form__input--textarea" onChange={inputChangeHandler}/>
 
-										<div className="form__hint form__hint--inverse">Please disclose whether you or the organisation who's behalf on which you are commenting has any past or current, direct or indirect links to, or receives funding from, the tobacco industry.</div>
+									{this.state.showOrganisationInput &&
+										<div className="form__group form__group--text">
+											<label htmlFor="organisationName" className="form__label">
+												Enter the name of your organisation
+											</label>
+											<input id="organisationName" name="organisationName" value={organisationName}
+														 className="form__input" type="text" onChange={inputChangeHandler}/>
+										</div>
+									}
+
+									<p>Do you or the organisation you represent have any links with the tobacco industry?</p>
+									<p>This includes:</p>
+									<ul>
+										<li>current or past links</li>
+										<li>direct or indirect links</li>
+										<li>receiving funding from the tobacco industry.</li>
+									</ul>
+
+									<div>
+
+										<div className="form__group form__group--radio form__group--inline">
+											<input
+												className="form__radio"
+												id="hasTobaccoLinks--true"
+												type="radio"
+												name="hasTobaccoLinks"
+												value="true"/>
+											<label
+												className="form__label form__label--radio"
+												htmlFor="hasTobaccoLinks--true">
+												Yes
+											</label>
+										</div>
+
+										<div className="form__group form__group--radio form__group--inline">
+											<input
+												className="form__radio"
+												id="hasTobaccoLinks--false"
+												type="radio"
+												name="hasTobaccoLinks"
+												value="false"/>
+											<label
+												className="form__label form__label--radio"
+												htmlFor="hasTobaccoLinks--false">
+												No
+											</label>
+										</div>
+
 									</div>
-								</fieldset>
 
-								{isAuthorised &&
-								<Fragment>
-									<h3 className="mt--0">Ready to submit?</h3>
-									<button
-										disabled={!validToSubmit}
-										className="btn btn--cta"
-										data-qa-sel="submit-comment-button"
-										onClick={submitConsultation}>
-										{userHasSubmitted ? "Responses submitted": "Yes, submit my response"}
-									</button>
-								</Fragment>
-								}
+									{this.state.showTobaccoInput &&
+										<div className="form__group form__group--textarea">
+											<label htmlFor="tobaccoDisclosure" className="form__label">
+												Please provide details
+											</label>
+											<textarea id="tobaccoDisclosure" name="tobaccoDisclosure" value={tobaccoDisclosure} className="form__input form__input--textarea" onChange={inputChangeHandler}/>
+										</div>
+									}
+
+									<p>Ready to submit?</p>
+
+									{isAuthorised &&
+									<Fragment>
+										<h3 className="mt--0">Ready to submit?</h3>
+										<button
+											disabled={!validToSubmit}
+											className="btn btn--cta"
+											data-qa-sel="submit-comment-button"
+											onClick={submitConsultation}>
+											{userHasSubmitted ? "Responses submitted": "Yes, submit my response"}
+										</button>
+									</Fragment>
+									}
+
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			}
-		</Fragment>
-	);
+				}
+			</Fragment>
+		)
+	}
 };
