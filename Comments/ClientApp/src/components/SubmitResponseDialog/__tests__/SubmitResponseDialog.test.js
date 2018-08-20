71875d315aa6a3285d6e197dad46e0c448f64332
Warren Keith <warren.keith@nice.org.uk>
2018-08-20 13:17:56 +0100

    EC-399 Tests finished

--- Comments/ClientApp/src/components/SubmitResponseDialog/__tests__/SubmitResponseDialog.test.js
+++ Comments/ClientApp/src/components/SubmitResponseDialog/__tests__/SubmitResponseDialog.test.js
@@ -12,18 +12,75 @@ describe("[ClientApp] ", () => {
 			userHasSubmitted: false,
 			validToSubmit: true,
 			submitConsultation: jest.fn(),
-			inputChangeHandler: jest.fn(),
+			fieldsChangeHandler: jest.fn(),
 			organisationName: "",
 			tobaccoDisclosure: "",
+			hasTobaccoLinks: "no",
+			respondingAsOrganisation: "no",
 		};
 
 		it("should fire parent change handler if the input values change", () => {
-			const wrapper = shallow(<SubmitResponseDialog {...fakeProps} />);
+			const localProps = fakeProps;
+			localProps.respondingAsOrganisation = "yes";
+			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
 			wrapper.find("input#organisationName").simulate("change", {target: {value: "h"}});
 			wrapper.find("input#organisationName").simulate("change", {target: {value: "i"}});
 			wrapper.find("input#organisationName").simulate("change", {target: {value: "!"}});
-			expect(fakeProps.inputChangeHandler.mock.calls[2][0].target.value).toBe("!");
-			expect(fakeProps.inputChangeHandler.mock.calls.length).toEqual(3);
+			expect(localProps.fieldsChangeHandler.mock.calls[2][0].target.value).toBe("!");
+			expect(localProps.fieldsChangeHandler.mock.calls.length).toEqual(3);
+		});
+
+		it("should not reveal text input unless radio button 'yes' is selected", () => {
+			const localProps = fakeProps;
+			localProps.respondingAsOrganisation = "no";
+			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(wrapper.find("input#organisationName").length).toEqual(0);
+			localProps.respondingAsOrganisation = "yes";
+			const newWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(newWrapper.find("input#organisationName").length).toEqual(1);
+		});
+
+		it("should not allow submission if the mandatory questions have not been answered", () => {
+			const localProps = fakeProps;
+			localProps.validToSubmit = true;
+
+			localProps.hasTobaccoLinks = ""; // not answered at all
+			localProps.respondingAsOrganisation = ""; // not answered at all
+			const wrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(wrapper.find("button").prop("disabled")).toBe(true);
+
+			localProps.hasTobaccoLinks = "no";
+			localProps.respondingAsOrganisation = ""; // not answered at all
+			const newWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(newWrapper.find("button").prop("disabled")).toBe(true);
+
+			localProps.hasTobaccoLinks = ""; // not answered at all
+			localProps.respondingAsOrganisation = "no";
+			const newerWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(newerWrapper.find("button").prop("disabled")).toBe(true);
+
+			localProps.hasTobaccoLinks = "no";
+			localProps.respondingAsOrganisation = "no";
+			const evenNewerWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(evenNewerWrapper.find("button").prop("disabled")).toBe(false);
+
+			localProps.hasTobaccoLinks = "yes";
+			localProps.respondingAsOrganisation = "no";
+			localProps.tobaccoDisclosure = "test";
+			const newestWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(newestWrapper.find("button").prop("disabled")).toBe(false);
+
+			localProps.hasTobaccoLinks = "no";
+			localProps.respondingAsOrganisation = "yes";
+			localProps.organisationName = "test";
+			const evenNewestWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(evenNewestWrapper.find("button").prop("disabled")).toBe(false);
+
+			localProps.hasTobaccoLinks = "no";
+			localProps.respondingAsOrganisation = "yes";
+			localProps.organisationName = "";
+			const lastWrapper = shallow(<SubmitResponseDialog {...localProps} />);
+			expect(lastWrapper.find("button").prop("disabled")).toBe(true);
 		});
 
 		it("should fire parent submit function when the submit button is clicked", () => {
