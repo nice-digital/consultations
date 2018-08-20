ac035ec7fd9b82dbb7f7bfb0d5e1bddb04f79b5e
Warren Keith <warren.keith@nice.org.uk>
2018-08-20 16:33:46 +0100

    EC-399 Amended markup from hero to panel

--- Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
+++ Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
@@ -38,140 +38,128 @@ export const SubmitResponseDialog = (props) => {
 					</div>
 				</div>
 				:
-				<div className="hero">
-					<div className="hero__container">
-						<div className="hero__body">
-							<div className="hero__copy">
+				<div className="panel">
+					<p className="lead">You are about to submit your final response to NICE</p>
 
-								<p className="hero__intro">You are about to submit your final response to NICE</p>
-								<p>After submission you won't be able to:</p>
-								<ul>
-									<li>edit your comments further</li>
-									<li>add any extra comments.</li>
-								</ul>
+					<p>You must answer the following questions before you submit.</p>
 
-								<p>You must answer the following questions before you submit</p>
+					<p><strong>Are you responding on behalf of an organisation?</strong></p>
 
-								<p>Are you responding on behalf of an organisation?</p>
+					<div>
 
-								<div>
-
-									<div className="form__group form__group--radio form__group--inline">
-										<input
-											className="form__radio"
-											id="respondingAsOrganisation--true"
-											type="radio"
-											name="respondingAsOrganisation"
-											checked={respondingAsOrganisation === "yes"}
-											onChange={fieldsChangeHandler}
-											value={"yes"}
-										/>
-										<label
-											className="form__label form__label--radio form__label--radio--inverted"
-											htmlFor="respondingAsOrganisation--true">
-											Yes
-										</label>
-									</div>
-
-									<div className="form__group form__group--radio form__group--inline">
-										<input
-											className="form__radio"
-											id="respondingAsOrganisation--false"
-											type="radio"
-											name="respondingAsOrganisation"
-											checked={respondingAsOrganisation === "no"}
-											onChange={fieldsChangeHandler}
-											value={"no"}
-										/>
-										<label
-											className="form__label form__label--radio form__label--radio--inverted"
-											htmlFor="respondingAsOrganisation--false">
-											No
-										</label>
-									</div>
-
-								</div>
-
-								{respondingAsOrganisation === "yes" &&
-								<div className="form__group form__group--text">
-									<label htmlFor="organisationName" className="form__label">
-										Enter the name of your organisation
-									</label>
-									<input id="organisationName" name="organisationName" value={organisationName}
-												 className="form__input" type="text" onChange={fieldsChangeHandler}/>
-								</div>
-								}
-
-								<p>Do you or the organisation you represent have any links with the tobacco industry?</p>
-								<p className="mb--0">This includes:</p>
-								<ul className="mt--0">
-									<li>current or past links</li>
-									<li>direct or indirect links</li>
-									<li>receiving funding from the tobacco industry.</li>
-								</ul>
-
-								<div>
-
-									<div className="form__group form__group--radio form__group--inline">
-										<input
-											className="form__radio"
-											id="hasTobaccoLinks--true"
-											type="radio"
-											name="hasTobaccoLinks"
-											value={"yes"}
-											checked={hasTobaccoLinks === "yes"}
-											onChange={fieldsChangeHandler}
-										/>
-										<label
-											className="form__label form__label--radio form__label--radio--inverted"
-											htmlFor="hasTobaccoLinks--true">
-											Yes
-										</label>
-									</div>
-
-									<div className="form__group form__group--radio form__group--inline">
-										<input
-											className="form__radio"
-											id="hasTobaccoLinks--false"
-											type="radio"
-											name="hasTobaccoLinks"
-											onChange={fieldsChangeHandler}
-											checked={hasTobaccoLinks === "no"}
-											value={"no"}/>
-										<label
-											className="form__label form__label--radio form__label--radio--inverted"
-											htmlFor="hasTobaccoLinks--false">
-											No
-										</label>
-									</div>
-
-								</div>
-
-								{hasTobaccoLinks === "yes" &&
-								<div className="form__group form__group--textarea">
-									<label htmlFor="tobaccoDisclosure" className="form__label">
-										Please provide details
-									</label>
-									<textarea id="tobaccoDisclosure" name="tobaccoDisclosure" value={tobaccoDisclosure} className="form__input form__input--textarea" onChange={fieldsChangeHandler}/>
-								</div>
-								}
-
-								{isAuthorised &&
-								<Fragment>
-									<h3 className="mt--0">Ready to submit?</h3>
-									<button
-										disabled={!validToSubmit || !mandatoryQuestionsAreValid()}
-										className="btn btn--cta"
-										data-qa-sel="submit-comment-button"
-										onClick={submitConsultation}>
-										{userHasSubmitted ? "Responses submitted": "Yes, submit my response"}
-									</button>
-								</Fragment>
-								}
-
-							</div>
+						<div className="form__group form__group--radio form__group--inline">
+							<input
+								className="form__radio"
+								id="respondingAsOrganisation--true"
+								type="radio"
+								name="respondingAsOrganisation"
+								checked={respondingAsOrganisation === "yes"}
+								onChange={fieldsChangeHandler}
+								value={"yes"}
+							/>
+							<label
+								className="form__label form__label--radio"
+								htmlFor="respondingAsOrganisation--true">
+								Yes
+							</label>
 						</div>
+
+						<div className="form__group form__group--radio form__group--inline">
+							<input
+								className="form__radio"
+								id="respondingAsOrganisation--false"
+								type="radio"
+								name="respondingAsOrganisation"
+								checked={respondingAsOrganisation === "no"}
+								onChange={fieldsChangeHandler}
+								value={"no"}
+							/>
+							<label
+								className="form__label form__label--radio"
+								htmlFor="respondingAsOrganisation--false">
+								No
+							</label>
+						</div>
+
 					</div>
+
+					{respondingAsOrganisation === "yes" &&
+					<div className="form__group form__group--text">
+						<label htmlFor="organisationName" className="form__label">
+							<strong>Enter the name of your organisation</strong>
+						</label>
+						<input id="organisationName" name="organisationName" value={organisationName}
+									 className="form__input" type="text" onChange={fieldsChangeHandler}/>
+					</div>
+					}
+
+					<p><strong>Do you or the organisation you represent have any links with the tobacco industry?</strong></p>
+					<p className="mb--0">This includes:</p>
+					<ul className="mt--0">
+						<li>current or past links</li>
+						<li>direct or indirect links</li>
+						<li>receiving funding from the tobacco industry.</li>
+					</ul>
+
+					<div>
+
+						<div className="form__group form__group--radio form__group--inline">
+							<input
+								className="form__radio"
+								id="hasTobaccoLinks--true"
+								type="radio"
+								name="hasTobaccoLinks"
+								value={"yes"}
+								checked={hasTobaccoLinks === "yes"}
+								onChange={fieldsChangeHandler}
+							/>
+							<label
+								className="form__label form__label--radio"
+								htmlFor="hasTobaccoLinks--true">
+								Yes
+							</label>
+						</div>
+
+						<div className="form__group form__group--radio form__group--inline">
+							<input
+								className="form__radio"
+								id="hasTobaccoLinks--false"
+								type="radio"
+								name="hasTobaccoLinks"
+								onChange={fieldsChangeHandler}
+								checked={hasTobaccoLinks === "no"}
+								value={"no"}/>
+							<label
+								className="form__label form__label--radio"
+								htmlFor="hasTobaccoLinks--false">
+								No
+							</label>
+						</div>
+
+					</div>
+
+					{hasTobaccoLinks === "yes" &&
+					<div className="form__group form__group--textarea">
+						<label htmlFor="tobaccoDisclosure" className="form__label">
+							<strong>Please provide details</strong>
+						</label>
+						<textarea id="tobaccoDisclosure" name="tobaccoDisclosure" value={tobaccoDisclosure} className="form__input form__input--textarea" onChange={fieldsChangeHandler}/>
+					</div>
+					}
+
+					{isAuthorised &&
+					<Fragment>
+						<p><strong>Now submit your response to NICE</strong></p>
+						<p>After submission you won't be able to edit your comments further or add any extra comments.</p>
+						<button
+							disabled={!validToSubmit || !mandatoryQuestionsAreValid()}
+							className="btn btn--cta"
+							data-qa-sel="submit-comment-button"
+							onClick={submitConsultation}>
+							{userHasSubmitted ? "Responses submitted": "Submit my response"}
+						</button>
+					</Fragment>
+					}
 				</div>
 			}
 		</Fragment>
