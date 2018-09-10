0aa80d170d1c309006465da84349a2de1d7b79d7
John Williams <jcwilliams@gmail.com>
2018-09-10 13:11:19 +0100

    EC-615 - updating snapshot

--- Comments/ClientApp/src/document-processing/process-document-html.js
+++ Comments/ClientApp/src/document-processing/process-document-html.js
@@ -1,26 +1,27 @@
-//this has been superseded by Chapter.js. leaving here commented for now, for reference. 
-// // @flow
+// @flow
 
-// import ReactHtmlParser from "react-html-parser";
-// import { nodeIsChapter, nodeIsInternalLink, nodeIsSection, nodeIsSubsection } from "./transforms/types";
-// import { processChapterSectionSubsection } from "./transforms/chapter-section-subsection";
-// import processInternalLink from "./transforms/internal-link";
+//NOTE: This has been superseded by Chapter.js. leaving here for now, for reference. 
 
-// // onNewCommentClick passed through from <Document />
-// export const processDocumentHtml = (incomingHtml: string, onNewCommentClick: Function, sourceURI: string, allowComments: boolean) => {
+import ReactHtmlParser from "react-html-parser";
+import { nodeIsChapter, nodeIsInternalLink, nodeIsSection, nodeIsSubsection } from "./transforms/types";
+import { processChapterSectionSubsection } from "./transforms/chapter-section-subsection";
+import processInternalLink from "./transforms/internal-link";
 
-// 	function transformHtml(node) {
-// 		const isSubsection = nodeIsSubsection(node);
-// 		if (nodeIsChapter(node) || nodeIsSection(node) || isSubsection) {
-// 			return processChapterSectionSubsection(node, onNewCommentClick, sourceURI, allowComments, isSubsection);
-// 		}
+// onNewCommentClick passed through from <Document />
+export const processDocumentHtml = (incomingHtml: string, onNewCommentClick: Function, sourceURI: string, allowComments: boolean) => {
 
-// 		if (nodeIsInternalLink(node)) {
-// 			return processInternalLink(node);
-// 		}
-// 	}
+	function transformHtml(node) {
+		const isSubsection = nodeIsSubsection(node);
+		if (nodeIsChapter(node) || nodeIsSection(node) || isSubsection) {
+			return processChapterSectionSubsection(node, onNewCommentClick, sourceURI, allowComments, isSubsection);
+		}
 
-// 	return ReactHtmlParser(incomingHtml, {
-// 		transform: transformHtml,
-// 	});
-// };
+		if (nodeIsInternalLink(node)) {
+			return processInternalLink(node);
+		}
+	}
+
+	return ReactHtmlParser(incomingHtml, {
+		transform: transformHtml,
+	});
+};
