3ec7998fc214a596e13f2c6cd9a6be8645d24a02
Warren Keith <warren.keith@nice.org.uk>
2018-10-01 11:48:12 +0100

    EC-677 Snapshot test for submitted page / updated enzyme and supporting scripts

---
+++ Comments/ClientApp/src/components/Submitted/__tests__/Submitted.test.js
@@ -0,0 +1,68 @@
+/* global jest */
+
+import React from "react";
+import { mount, shallow } from "enzyme";
+import { Submitted } from "../Submitted";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { MemoryRouter } from "react-router";
+// import { LiveAnnouncer } from "react-aria-live";
+import ConsultationData from "./Consultation.json";
+import { nextTick } from "../../../helpers/utils";
+import toJson from "enzyme-to-json";
+
+jest.mock("../../../context/UserContext", () => {
+	return {
+		UserContext: {
+			Consumer: (props) => {
+				return props.children({
+					isAuthorised: true,
+				});
+			},
+		},
+	};
+});
+
+describe("[ClientApp] ", () => {
+	describe("Submitted page", () => {
+
+		const fakeProps = {
+			match: {
+				params: {
+					consultationId: 1,
+				},
+			},
+		};
+
+		it("should match snapshot with supplied data", () => {
+			const mock = new MockAdapter(axios);
+
+			const wrapper = mount(
+				<MemoryRouter>
+					<Submitted {...fakeProps} />
+				</MemoryRouter>
+			);
+
+			let consultationPromise = new Promise(resolve => {
+				mock
+					.onAny()
+					.reply(() => {
+						resolve();
+						return [200, ConsultationData];
+					});
+			});
+
+			return Promise.all([
+				consultationPromise,
+			]).then(async () => {
+				await nextTick();
+				wrapper.update();
+
+				expect(
+					toJson(wrapper)
+				).toMatchSnapshot();
+			});
+		});
+
+	});
+});
