72155228971a7cc5d691b818cdbd13de5034a645
John Williams <jcwilliams@gmail.com>
2018-10-12 12:29:36 +0100

    EC-329 - adding failing tests

--- Comments.Test/IntegrationTests/API/ConsultationList/ConsultationListTests.cs
+++ Comments.Test/IntegrationTests/API/ConsultationList/ConsultationListTests.cs
@@ -1,8 +1,10 @@
+using System.Net;
 using Comments.Test.Infrastructure;
 using System.Threading.Tasks;
 using Comments.Configuration;
 using Comments.Services;
 using NICE.Feeds.Tests.Infrastructure;
+using Shouldly;
 using Xunit;
 using TestBase = Comments.Test.Infrastructure.TestBase;
 
@@ -29,5 +31,48 @@ namespace Comments.Test.IntegrationTests.API.ConsultationList
             // Assert
             responseString.ShouldMatchApproved();
         }
+	}
+
+
+	public class ConsultationListNotAuthTests : TestBase
+	{
+		public ConsultationListNotAuthTests() : base(TestUserType.NotAuthenticated, Feed.ConsultationCommentsListMultiple)
+		{
+			AppSettings.ConsultationListConfig = TestAppSettings.GetConsultationListConfig();
+			AppSettings.Feed = TestAppSettings.GetFeedConfig();
+		}
+
+		[Fact]
+		public async Task Get_Consultation_Feed_Returns_Populated_Feed()
+		{
+			//Arrange
+
+			// Act
+			var response = await _client.GetAsync("/consultations/api/ConsultationList?Status=Open");
+
+			// Assert
+			response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+		}
+	}
+
+	public class ConsultationListAuthTests : TestBase
+	{
+		public ConsultationListAuthTests() : base(TestUserType.Authenticated, Feed.ConsultationCommentsListMultiple)
+		{
+			AppSettings.ConsultationListConfig = TestAppSettings.GetConsultationListConfig();
+			AppSettings.Feed = TestAppSettings.GetFeedConfig();
+		}
+
+		[Fact]
+		public async Task Get_Consultation_Feed_Returns_Populated_Feed()
+		{
+			//Arrange
+
+			// Act
+			var response = await _client.GetAsync("/consultations/api/ConsultationList?Status=Open");
+
+			// Assert
+			response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+		}
 	}
 }
