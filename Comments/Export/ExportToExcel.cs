d6a15e76c151a1dd6050f0487edfc67ee31c0c52
Sue Garner <Sue.Garner@nice.org.uk>
2018-12-05 10:41:44 +0000

    EC-748 Added Title

--- Comments/Export/ExportToExcel.cs
+++ Comments/Export/ExportToExcel.cs
@@ -43,6 +43,111 @@ namespace Comments.Export
 			return stream;
 		}
 
+		private Columns SetUpColumns()
+		{
+			Columns columns = new Columns(
+				new Column // User
+				{
+					Min = 1,
+					Max = 14,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Email Address
+				{
+					Min = 2,
+					Max = 2,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Consultation Name
+				{
+					Min = 3,
+					Max = 3,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Document Name
+				{
+					Min = 4,
+					Max = 4,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Chapter Name
+				{
+					Min = 5,
+					Max = 5,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Section
+				{
+					Min = 6,
+					Max = 6,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Selected text
+				{
+					Min = 7,
+					Max = 7,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Comment
+				{
+					Min = 8,
+					Max = 8,
+					Width = 50,
+					CustomWidth = true
+				},
+				new Column // Question Text
+				{
+					Min = 9,
+					Max = 9,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Answer Text
+				{
+					Min = 10,
+					Max = 10,
+					Width = 50,
+					CustomWidth = true
+				},
+				new Column // Represent An Organisation
+				{
+					Min = 11,
+					Max = 11,
+					Width = 20,
+					CustomWidth = true
+				},
+				new Column // Organisation Name
+				{
+					Min = 12,
+					Max = 12,
+					Width = 25,
+					CustomWidth = true
+				},
+				new Column // Has Tobacco Links
+				{
+					Min = 13,
+					Max = 13,
+					Width = 20,
+					CustomWidth = true
+				},
+				new Column // Tobacco Link Details
+				{
+					Min = 14,
+					Max = 14,
+					Width = 25,
+					CustomWidth = true
+				});
+
+			return columns;
+		}
+
 		private void AppendHeaderRow(SheetData sheet)
 		{
 			var headerRow = new Row();
@@ -52,71 +157,85 @@ namespace Comments.Export
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("User");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Email Address");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Consultation Name");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Document Name");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Chapter Name");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Section");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Selected Text");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Comment");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Question Text");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Answer Text");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Represents An Organisation");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue(" Organisation");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Has Tobacco Links");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			cell = new Cell();
 			cell.DataType = CellValues.String;
 			cell.CellValue = new CellValue("Tobacco Link Details");
+			cell.StyleIndex = 2;
 			headerRow.AppendChild(cell);
 
 			sheet.AppendChild(headerRow);
@@ -135,71 +254,85 @@ namespace Comments.Export
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.UserName);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Email);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.ConsultationName);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.DocumentName);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.ChapterTitle);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Section);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Quote);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Comment);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 				
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Question);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.Answer);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.RepresentsOrganisation == true ? "Yes" : row.RepresentsOrganisation == false ? "No" : null);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.OrganisationName);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.HasTobaccoLinks == true ? "Yes" : row.HasTobaccoLinks == false ? "No" : null);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				cell = new Cell();
 				cell.DataType = CellValues.String;
 				cell.CellValue = new CellValue(row.TobaccoIndustryDetails);
+				cell.StyleIndex = 1;
 				dataRow.AppendChild(cell);
 
 				sheet.AppendChild(dataRow);
@@ -292,6 +425,57 @@ namespace Comments.Export
 			return orderedData;
 		}
 
+		private Stylesheet CreateStyleSheet()
+		{
+			Stylesheet styleSheet = new Stylesheet();
+
+			Fonts fonts = new Fonts(
+				new Font( // Index 0 - default
+					new FontSize() { Val = 10 }
+
+				),
+				new Font( // Index 1 - header
+					new FontSize() { Val = 10 },
+					new Bold(),
+					new Color() { Rgb = "FFFFFF" }
+
+				),
+				new Font( // Index 1 - Title
+					new FontSize() { Val = 16 },
+					new Bold(),
+					new Color() { Rgb = "000000" }
+
+				));
+
+			Fills fills = new Fills(
+				new Fill(new PatternFill() { PatternType = PatternValues.None }), // Index 0 - default
+				new Fill(new PatternFill() { PatternType = PatternValues.Gray125 }), // Index 1 - default
+				new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "66666666" } })
+					{ PatternType = PatternValues.Solid }) // Index 2 - header
+			);
+
+			Borders borders = new Borders(
+				new Border(), // index 0 default
+				new Border( // index 1 black border
+					new LeftBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+					new RightBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+					new TopBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+					new BottomBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+					new DiagonalBorder())
+			);
+
+			CellFormats cellFormats = new CellFormats(
+				new CellFormat(), // default
+				new CellFormat { FontId = 0, FillId = 0, BorderId = 1, ApplyBorder = true, Alignment = new Alignment() { WrapText = true } }, // body
+				new CellFormat { FontId = 1, FillId = 2, BorderId = 1, ApplyFill = true, Alignment = new Alignment() { WrapText = true } }, // header
+				new CellFormat { FontId = 2, FillId = 0, BorderId = 0, ApplyFill = true, Alignment = new Alignment() { WrapText = false } } // title
+			);
+
+			styleSheet = new Stylesheet(fonts, fills, borders, cellFormats);
+
+			return styleSheet;
+		}
+
 		private void CreateSheet(SpreadsheetDocument spreadsheetDocument, IEnumerable<Models.Comment> comments, IEnumerable<Models.Answer> answers, IEnumerable<Models.Question> questions)
 		{
 			// Add a WorkbookPart to the document.
@@ -300,12 +484,17 @@ namespace Comments.Export
 
 			// Add a WorksheetPart to the WorkbookPart.
 			WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
-			SheetData sheetData = new SheetData();
-			worksheetPart.Worksheet = new Worksheet(sheetData);
+			worksheetPart.Worksheet = new Worksheet();
+
+			// Add a Style to the workbook part
+			WorkbookStylesPart stylesPart = workbookpart.AddNewPart<WorkbookStylesPart>();
+			stylesPart.Stylesheet = CreateStyleSheet();
+			stylesPart.Stylesheet.Save();
+
+			worksheetPart.Worksheet.AppendChild(SetUpColumns());
 
 			// Add Sheets to the Workbook.
-			Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
-			//Sheets sheets = workbookpart.Workbook.GetFirstChild<Sheets>();
+			Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
 
 			// Append a new worksheet and associate it with the spreadsheetDocument.
 			Sheet sheet = new Sheet()
@@ -315,16 +504,51 @@ namespace Comments.Export
 				Name = "Comments"
 			};
 			sheets.Append(sheet);
+			
+			workbookpart.Workbook.Save();
+
+			// Add data to the worksheet
+			SheetData sheetData = worksheetPart.Worksheet.AppendChild(new SheetData());
+
+			var ConsultationTitle = GetConsultationTitle(comments, questions);
+			AppendTitleRow(sheetData, ConsultationTitle);
 
 			AppendHeaderRow(sheetData);
 
 			AppendDataRow(sheetData, comments, answers, questions);
 
-			workbookpart.WorksheetParts.First().Worksheet.Save();
-			workbookpart.Workbook.Save();
+			worksheetPart.Worksheet.Save();
 
 			// Close the document.
 			spreadsheetDocument.Close();
+		}
+
+		private string GetConsultationTitle(IEnumerable<Models.Comment> comments, IEnumerable<Question> questions)
+		{
+			if (comments.Count() > 0)
+				return _exportService.GetConsultationName(comments.First().Location);
+				
+			if (questions.Count() > 0)
+				return _exportService.GetConsultationName(questions.First().Location);
+
+			return "";
+		}
+
+		private void AppendTitleRow(SheetData sheet, string consultationTitle)
+		{
+			var titleRow = new Row();
+
+			Cell cell = new Cell();
+			cell.DataType = CellValues.String;
+			cell.CellValue = new CellValue(consultationTitle);
+			cell.StyleIndex = 3;
+			titleRow.AppendChild(cell);
+
+			sheet.AppendChild(titleRow);
+
+			var blankRow = new Row();
+			sheet.AppendChild(blankRow);
+
 		}
 	}
 }
