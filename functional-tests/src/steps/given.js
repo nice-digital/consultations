281f7afb3d1eae5033be733987440b536c6c684c
Cheguegallagher <martin.gallagher@nice.org.uk>
2018-12-14 12:50:41 +0000

    refactoring subsection feature file

--- functional-tests/src/steps/given.js
+++ functional-tests/src/steps/given.js
@@ -11,37 +11,49 @@ Given(
 Given(
 	/^I comment on a Document$/,
 	() => {
-		browser.pause(1000);// And I pause for 1000ms
-		browser.click("[data-qa-sel='comment-on-consultation-document']")// When I click on the button "[data-qa-sel='comment-on-consultation-document']"
-		browser.pause(1000);// And I pause for 1000ms
-		browser.waitForVisible("body [data-qa-sel='comment-box-title']", 1000)// Then I wait on element "body [data-qa-sel='comment-box-title']" for 10000ms to be visible
+		browser.pause(1000);
+		browser.click("[data-qa-sel='comment-on-consultation-document']");
+		browser.pause(1000);
+		browser.waitForVisible("body [data-qa-sel='comment-box-title']", 1000);
 	}
 );
 
 Given(
 	/^I comment on a Chapter$/,
 	() => {
-		browser.pause(1000);// And I pause for 1000ms
-		browser.waitForVisible(".document-comment-container");// When I wait on element ".document-comment-container" to be visible
-		browser.pause(1000);// And I pause for 1000ms
-		browser.click(".chapter > .title [data-qa-sel='in-text-comment-button']");// When I click on the button ".chapter > .title [data-qa-sel='in-text-comment-button']"
-		browser.pause(1000);// And I pause for 1000ms
-		browser.waitForVisible("body [data-qa-sel='comment-box-title']");// Then I wait on element "body [data-qa-sel='comment-box-title']" to be visible
-		// Then I expect that element "[data-qa-sel='comment-box-title']" contains the text "chapter"
+		browser.pause(1000);
+		browser.waitForVisible(".document-comment-container");
+		browser.pause(1000);
+		browser.click(".chapter > .title [data-qa-sel='in-text-comment-button']");
+		browser.pause(1000);
+		browser.waitForVisible("body [data-qa-sel='comment-box-title']");
 	}
 );
 
 Given(
 	/^I comment on a Section$/,
 	() => {
-		browser.pause(1000);// And I pause for 1000ms
-		browser.click("[data-qa-sel='nav-list-item']:nth-of-type(4)");// When I click on the button "[data-qa-sel='nav-list-item']:nth-of-type(4)"
-		browser.waitForVisible(".document-comment-container");// When I wait on element ".document-comment-container" to be visible
-		browser.pause(1000);// And I pause for 1000ms
-		browser.waitForVisible(".section:first-of-type > .title [data-qa-sel='in-text-comment-button']");// When I wait on element ".section:first-of-type > .title [data-qa-sel='in-text-comment-button']" to be visible
-		browser.pause(1000);// And I pause for 1000ms
-		browser.click(".section:first-of-type > .title [data-qa-sel='in-text-comment-button']");// When I click on the button ".section:first-of-type > .title [data-qa-sel='in-text-comment-button']"
-		browser.pause(1000);// And I pause for 1000ms
-		browser.waitForVisible("body [data-qa-sel='comment-box-title']");// Then I wait on element "body [data-qa-sel='comment-box-title']" to be visible
+		browser.pause(1000);
+		browser.click("[data-qa-sel='nav-list-item']:nth-of-type(4)");
+		browser.waitForVisible(".document-comment-container");
+		browser.pause(1000);
+		browser.waitForVisible(".section:first-of-type > .title [data-qa-sel='in-text-comment-button']");
+		browser.pause(1000);
+		browser.click(".section:first-of-type > .title [data-qa-sel='in-text-comment-button']");
+		browser.pause(1000);
+		browser.waitForVisible("body [data-qa-sel='comment-box-title']");
 	}
 );
+
+Given(
+	/^I comment on a Sub-section$/,
+	() => {
+		browser.pause(1000);
+		browser.waitForVisible(".section:first-of-type > [data-qa-sel='in-text-comment-button']");
+		browser.click(".section:first-of-type > [data-qa-sel='in-text-comment-button']");
+		browser.waitForVisible(".document-comment-container");
+		browser.pause(1000);
+		browser.waitForVisible("body [data-qa-sel='comment-box-title']");
+		browser.pause(1000);
+	}
+)
