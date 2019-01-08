f6a0f7878c8f0a942552eb6ad90a5005a7ef371d
Sue Garner <Sue.Garner@nice.org.uk>
2019-01-08 14:18:48 +0000

    EC-768 Added sheet relationship

--- Comments/Export/ExportToExcel.cs
+++ Comments/Export/ExportToExcel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Packaging;
 using System.Linq;
 using Comments.Models;
 using Comments.Services;
@@ -499,15 +500,24 @@ namespace Comments.Export
 			// Add Sheets to the Workbook.
 			Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
 
+			//******************Ipad stuff**************************//
+			string relationshipId = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart);
+			string relationshipType = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet";
+			spreadsheetDocument.Package.GetPart(spreadsheetDocument.WorkbookPart.Uri).CreateRelationship(new Uri(worksheetPart.Uri.OriginalString.Replace("/xl/", String.Empty).Trim(), UriKind.Relative), TargetMode.Internal, relationshipType);
+			spreadsheetDocument.Package.GetPart(spreadsheetDocument.WorkbookPart.Uri).DeleteRelationship(relationshipId);
+			PackageRelationshipCollection sheetRelationships = spreadsheetDocument.Package.GetPart(spreadsheetDocument.WorkbookPart.Uri).GetRelationshipsByType(relationshipType);
+			relationshipId = sheetRelationships.Where(f => f.TargetUri.OriginalString == worksheetPart.Uri.OriginalString.Replace("/xl/", String.Empty).Trim()).Single().Id;
+			//******************Ipad stuff**************************//
+
 			// Append a new worksheet and associate it with the spreadsheetDocument.
 			Sheet sheet = new Sheet()
 			{
-				Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+				Id = relationshipId, //spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
 				SheetId = 1,
 				Name = "Comments"
 			};
 			sheets.Append(sheet);
-			
+
 			workbookpart.Workbook.Save();
 
 			// Add data to the worksheet
