967410cf85af1283fc76f840994113fad66c0798
Warren Keith <warren.keith@nice.org.uk>
2019-01-09 11:36:32 +0000

    EC-729 Replaced ARIA attributes to replace fieldset

--- Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
+++ Comments/ClientApp/src/components/SubmitResponseDialog/SubmitResponseDialog.js
@@ -36,126 +36,118 @@ export const SubmitResponseDialog = (props) => {
 
 			<p>You must answer the following questions before you submit.</p>
 
-			<fieldset>
-				<legend>
-					Are you responding on behalf of an organisation?
-				</legend>
-				<div role="radiogroup" aria-label="Are you responding on behalf of an organisation?">
-					<div className="form__group form__group--radio form__group--inline">
-						<input
-							className="form__radio"
-							id="respondingAsOrganisation--true"
-							type="radio"
-							name="respondingAsOrganisation"
-							checked={respondingAsOrganisation === "yes"}
-							onChange={fieldsChangeHandler}
-							value={"yes"}
-						/>
-						<label
-							className="form__label form__label--radio"
-							htmlFor="respondingAsOrganisation--true">
-							Yes
-						</label>
-					</div>
+			<p><strong>Are you responding on behalf of an organisation?</strong></p>
 
-					<div className="form__group form__group--radio form__group--inline">
-						<input
-							className="form__radio"
-							id="respondingAsOrganisation--false"
-							type="radio"
-							name="respondingAsOrganisation"
-							checked={respondingAsOrganisation === "no"}
-							onChange={fieldsChangeHandler}
-							value={"no"}
-						/>
-						<label
-							data-qa-sel="respond-no-responding-as-org"
-							className="form__label form__label--radio"
-							htmlFor="respondingAsOrganisation--false">
-							No
-						</label>
-					</div>
-				</div>
-				{respondingAsOrganisation === "yes" &&
-				<div className="form__group form__group--text">
-
-					<label htmlFor="organisationName" className="form__label">
-						<strong>Enter the name of your organisation</strong>
+			<div role="radiogroup" aria-label="Are you responding on behalf of an organisation?">
+				<div className="form__group form__group--radio form__group--inline">
+					<input
+						className="form__radio"
+						id="respondingAsOrganisation--true"
+						type="radio"
+						name="respondingAsOrganisation"
+						checked={respondingAsOrganisation === "yes"}
+						onChange={fieldsChangeHandler}
+						value={"yes"}
+					/>
+					<label
+						className="form__label form__label--radio"
+						htmlFor="respondingAsOrganisation--true">
+								Yes
 					</label>
-
-					<input data-hj-whitelist id="organisationName" name="organisationName" value={organisationName}
-								 className="form__input" type="text" onChange={fieldsChangeHandler}/>
-				</div>
-				}
-
-			</fieldset>
-
-
-
-
-
-			<fieldset>
-				<legend>
-					Do you or the organisation you represent have any links with the tobacco industry?
-				</legend>
-				<div role="radiogroup" aria-label="Do you or the organisation you represent have any links with the tobacco industry?">
-
-					<div className="form__group form__group--radio form__group--inline">
-						<input
-							className="form__radio"
-							id="hasTobaccoLinks--true"
-							type="radio"
-							name="hasTobaccoLinks"
-							value={"yes"}
-							checked={hasTobaccoLinks === "yes"}
-							onChange={fieldsChangeHandler}
-						/>
-						<label
-							className="form__label form__label--radio"
-							htmlFor="hasTobaccoLinks--true">
-							Yes
-						</label>
-					</div>
-
-					<div className="form__group form__group--radio form__group--inline">
-						<input
-							className="form__radio"
-							id="hasTobaccoLinks--false"
-							type="radio"
-							name="hasTobaccoLinks"
-							onChange={fieldsChangeHandler}
-							checked={hasTobaccoLinks === "no"}
-							value={"no"}
-						/>
-						<label
-							data-qa-sel="respond-no-has-tobac-links"
-							className="form__label form__label--radio"
-							htmlFor="hasTobaccoLinks--false">
-							No
-						</label>
-					</div>
-
 				</div>
 
-				{hasTobaccoLinks === "yes" &&
-				<div className="form__group form__group--textarea">
-					<label htmlFor="tobaccoDisclosure" className="form__label">
-						<strong>Please provide details</strong>
+				<div className="form__group form__group--radio form__group--inline">
+					<input
+						className="form__radio"
+						id="respondingAsOrganisation--false"
+						type="radio"
+						name="respondingAsOrganisation"
+						checked={respondingAsOrganisation === "no"}
+						onChange={fieldsChangeHandler}
+						value={"no"}
+					/>
+					<label
+						data-qa-sel="respond-no-responding-as-org"
+						className="form__label form__label--radio"
+						htmlFor="respondingAsOrganisation--false">
+								No
 					</label>
-					<textarea
-						data-hj-whitelist
-						id="tobaccoDisclosure"
-						name="tobaccoDisclosure"
-						value={tobaccoDisclosure}
-						className="form__input form__input--textarea"
-						onChange={fieldsChangeHandler}/>
 				</div>
-				}
+			</div>
 
-			</fieldset>
+			{respondingAsOrganisation === "yes" &&
+					<div className="form__group form__group--text">
 
+						<label htmlFor="organisationName" className="form__label">
+							<strong>Enter the name of your organisation</strong>
+						</label>
 
+						<input data-hj-whitelist id="organisationName" name="organisationName" value={organisationName}
+									 className="form__input" type="text" onChange={fieldsChangeHandler}/>
+					</div>
+			}
 
+			<p><strong>Do you or the organisation you represent have any links with the tobacco industry?</strong></p>
+			<p className="mb--0">This includes:</p>
+			<ul className="mt--0">
+				<li>current or past links</li>
+				<li>direct or indirect links</li>
+				<li>receiving funding from the tobacco industry.</li>
+			</ul>
+
+			<div role="radiogroup" aria-label="Do you or the organisation you represent have any links with the tobacco industry?">
+
+				<div className="form__group form__group--radio form__group--inline">
+					<input
+						className="form__radio"
+						id="hasTobaccoLinks--true"
+						type="radio"
+						name="hasTobaccoLinks"
+						value={"yes"}
+						checked={hasTobaccoLinks === "yes"}
+						onChange={fieldsChangeHandler}
+					/>
+					<label
+						className="form__label form__label--radio"
+						htmlFor="hasTobaccoLinks--true">
+								Yes
+					</label>
+				</div>
+
+				<div className="form__group form__group--radio form__group--inline">
+					<input
+						className="form__radio"
+						id="hasTobaccoLinks--false"
+						type="radio"
+						name="hasTobaccoLinks"
+						onChange={fieldsChangeHandler}
+						checked={hasTobaccoLinks === "no"}
+						value={"no"}
+					/>
+					<label
+						data-qa-sel="respond-no-has-tobac-links"
+						className="form__label form__label--radio"
+						htmlFor="hasTobaccoLinks--false">
+								No
+					</label>
+				</div>
+
+			</div>
+
+			{hasTobaccoLinks === "yes" &&
+					<div className="form__group form__group--textarea">
+						<label htmlFor="tobaccoDisclosure" className="form__label">
+							<strong>Please provide details</strong>
+						</label>
+						<textarea
+							data-hj-whitelist
+							id="tobaccoDisclosure"
+							name="tobaccoDisclosure"
+							value={tobaccoDisclosure}
+							className="form__input form__input--textarea"
+							onChange={fieldsChangeHandler}/>
+					</div>
+			}
 
 			{isAuthorised &&
 					<Fragment>
