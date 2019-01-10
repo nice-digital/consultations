56dfbe483a8b4ac4fcdf50dd8cb67a7e9ca8fc12
Warren Keith <warren.keith@nice.org.uk>
2019-01-10 16:43:40 +0000

    EC-612 Add 3rd column to tutorial and larger CTA for review page

--- Comments/ClientApp/src/components/Tutorial/Tutorial.js
+++ Comments/ClientApp/src/components/Tutorial/Tutorial.js
@@ -55,24 +55,29 @@ export class Tutorial extends Component<PropsType, StateType> {
 						onClick={() => this.handleClick(isPanelVisible)}>
 						{isPanelVisible ? "Hide how to comment" : "Show how to comment"}
 					</button>
-					{isPanelVisible ?
-						<div>
-							<h1 className="pt--c">How to comment</h1>
-							<div className="grid">
-								<div data-g="12 sm:6 lg:4">
-									<p className="mt--0">Use the icon next to chapters, subsections and recommendations to comment on
-										them</p>
-									<img className="Tutorial__buttonImage" src="images/tutorial_graphic_800.png"
-											 alt="Mouse pointer hovering over commenting icon"/>
-								</div>
-								<div data-g="12 sm:6 lg:4">
-									<p className="mt--0 mb--0">Highlight a selection of text using the cursor to make a comment on it</p>
-									<img src="images/text-selection.gif"
-											 alt="Animation showing an example of highlighting text and clicking comment"/>
-								</div>
+					{isPanelVisible &&
+					<div>
+						<h1 className="pt--c">How to comment</h1>
+						<div className="grid">
+							<div data-g="12 sm:4">
+								<p className="mt--0">Use the icon next to chapters, subsections and recommendations to comment on
+									them</p>
+								<img className="Tutorial__buttonImage" src="images/tutorial-1.png"
+										 alt="Mouse pointer hovering over commenting icon"/>
+							</div>
+							<div data-g="12 sm:4">
+								<p className="mt--0">You can also highlight a selection of text using the cursor to make a comment on it</p>
+								<img className="Tutorial__buttonImage" src="images/tutorial-2.gif"
+										 alt="Animation showing an example of highlighting text and clicking comment"/>
+							</div>
+							<div data-g="12 sm:4">
+								<p className="mt--0">Review your comments or answers before submitting your response</p>
+								<img className="Tutorial__buttonImage" src="images/tutorial-3.png"
+										 alt="Indication of how to get to the review page"/>
 							</div>
 						</div>
-						: null}
+					</div>
+					}
 				</div>
 			</aside>
 		);
