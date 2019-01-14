d732d8086f9797f06f00a564c804c2606313bd8e
Cheguegallagher <martin.gallagher@nice.org.uk>
2019-01-14 10:07:55 +0000

    joining qa selector and nthChild selector into one

--- functional-tests/src/support/selectors.js
+++ functional-tests/src/support/selectors.js
@@ -1,25 +1,25 @@
 const toDataQASelAttr = (attrValue) => `[data-qa-sel='${attrValue}']`;
 const toNthChildAttr = (attrValue) => `.CommentBox:nth-child(${attrValue})`;
-const steve = (childIndex, attrValue) => toNthChildAttr(childIndex) + ' ' + toDataQASelAttr(attrValue);
+const toChildAndQASel = (childIndex, attrValue) => toNthChildAttr(childIndex) + ' ' + toDataQASelAttr(attrValue);
 
 export default {
 	documentPage: {
 		commentBoxTitle: toDataQASelAttr("comment-box-title"),
-		firstCommentTextArea: steve(1, "Comment-text-area"),
-		secondCommentTextArea: toNthChildAttr(2) + ' ' + toDataQASelAttr("Comment-text-area"),
-		thirdCommentTextArea: toNthChildAttr(3) + ' ' + toDataQASelAttr("Comment-text-area"),
+		firstCommentTextArea: toChildAndQASel(1, "Comment-text-area"),
+		secondCommentTextArea: toChildAndQASel(2, "Comment-text-area"),
+		thirdCommentTextArea: toChildAndQASel(3, "Comment-text-area"),
 		commentTextArea:toDataQASelAttr("Comment-text-area"),
 		firstCommentTextArea:"#Comment-1",
 		submitButton:toDataQASelAttr("submit-button"),
-		saveIndicator: toNthChildAttr(1) + ' ' + ".CommentBox__savedIndicator",
+		saveIndicator: toChildAndQASel(1, ".CommentBox__savedIndicator"),
 		deletebutton: toDataQASelAttr("delete-comment-button"),
 		reviewAllButton: toDataQASelAttr("review-all-comments")
 	},
 	reviewPage: {
 		commentTextArea: toDataQASelAttr("Comment-text-area"),
-		firstCommentTextArea: toNthChildAttr(1) + ' ' + toDataQASelAttr("Comment-text-area"),
-		secondCommentTextArea: toNthChildAttr(2) + ' ' + toDataQASelAttr("Comment-text-area"),
-		thirdCommentTextArea: toNthChildAttr(3) + ' ' + toDataQASelAttr("Comment-text-area"),
+		firstCommentTextArea: toChildAndQASel(1, "Comment-text-area"),
+		secondCommentTextArea: toChildAndQASel(2, "Comment-text-area"),
+		thirdCommentTextArea: toChildAndQASel(3, "Comment-text-area"),
 		answerNoRepresentOrg: toDataQASelAttr("respond-no-responding-as-org"),
 		answerNoTobacLink: toDataQASelAttr("respond-no-has-tobac-links"),
 		submitResponseButton: toDataQASelAttr("submit-comment-button"),
