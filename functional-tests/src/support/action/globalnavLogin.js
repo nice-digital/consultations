feb827d93df8eca3fd1f02089fbadb9291e4bec8
John Williams <jcwilliams@gmail.com>
2019-06-06 09:17:22 +0100

    EC-781 - version bump of cucumber steps with the global nav login function moved there.

--- functional-tests/src/support/action/globalnavLogin.js
+++ functional-tests/src/support/action/globalnavLogin.js
@@ -1,23 +1,6 @@
-import login from "@nice-digital/wdio-cucumber-steps/lib/support/action/login";
+import globalnavLogin from "@nice-digital/wdio-cucumber-steps/lib/support/action/globalnavLogin";
 import waitFor from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitFor";
 import selectors from "../selectors";
-
-const globalnavLogin = (username, password) => { //todo: potentially - move this function to wdio cucumber steps.
-	// If you are already logged in
-	if (browser.getCookie("__nrpa_2.2")) {
-		return;
-	}
-
-	var headerMenuShown = browser.isVisible("#header-menu-button");
-	if (headerMenuShown) {
-		browser.waitForExist("header[aria-label='Site header']");
-		browser.click("#header-menu-button");
-		browser.click("#header-menu a[href*='accounts.nice.org.uk/signin']");
-	} else {
-		browser.click("#header-menu-button+* a[href*='accounts.nice.org.uk/signin']");
-	}	
-	login(username, password);
-};
 
 export const Login = (username, password) => {
 	browser.refresh();
