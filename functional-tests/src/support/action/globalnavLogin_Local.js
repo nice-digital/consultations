2ddf229cc3ca11de355dfb8a6e58196f4c720f1e
John Williams <jcwilliams@gmail.com>
2019-06-21 08:58:52 +0100

    EC-781 - resizing the viewport

--- functional-tests/src/support/action/globalnavLogin_Local.js
+++ functional-tests/src/support/action/globalnavLogin_Local.js
@@ -15,15 +15,37 @@ module.exports = (username, password) => {
 		browser.click("#cookie-banner-description + button"); //dismiss cookie notification, if shown.
 	}	
 
+	// var runInBrowser = function(argument) { //this hacky function is here to click things in the browser that selenium can't.
+	// 	argument.click();
+	// };
+
+	browser.setViewportSize({
+        width: 1366,
+        height: 768
+    });
+
 	var headerMenuShown = browser.isVisible("#header-menu-button"); //the global nav shows a "menu" button for small screens, or a "sign in" button for big screens.
 	if (headerMenuShown) { //if a small screen
 		browser.click("#header-menu-button"); //click the menu button
 		scroll(selectors.documentPage.globalNavSiginMenuSignInButton); //scroll to the signin button in the menu
 		waitForVisible(selectors.documentPage.globalNavSiginMenuSignInButton); 
+
+		var signinButtonInMenu = browser.$(selectors.documentPage.globalNavSiginMenuSignInButton);
+		console.log("signinButtonInMenu:")
+		console.log(signinButtonInMenu);
+		//browser.execute(runInBrowser, signinButtonInMenu);
+
 		browser.click(selectors.documentPage.globalNavSiginMenuSignInButton);
 	} else {
 		scroll(selectors.documentPage.globalNavSigin);
 		waitForVisible(selectors.documentPage.globalNavSigin);
+
+		var signinButton = browser.$(selectors.documentPage.globalNavSigin);
+		console.log("signinButton:")
+		console.log(signinButton);
+
+		//browser.execute(runInBrowser, signinButton);
+
 		browser.click(selectors.documentPage.globalNavSigin);
 	}
 	login(username, password);
