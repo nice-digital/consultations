e3dd5753c3941d963adc2bd0d78e7c732949cd07
Sue Garner <Sue.Garner@nice.org.uk>
2019-07-09 11:41:37 +0100

    EC-688 Created EF Migration script

--- Comments/Migrations/ConsultationsContextModelSnapshot.cs
+++ Comments/Migrations/ConsultationsContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Comments.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
+                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -195,6 +195,10 @@ namespace Comments.Migrations
                     b.HasKey("QuestionTypeId");
 
                     b.ToTable("QuestionType");
+
+                    b.HasData(
+                        new { QuestionTypeId = 99, Description = "A yes / no answer without a text answer", HasBooleanAnswer = true, HasTextAnswer = true }
+                    );
                 });
 
             modelBuilder.Entity("Comments.Models.Status", b =>
