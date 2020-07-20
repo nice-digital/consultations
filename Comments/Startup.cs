432fb25203d057305ce7e4c5cc612d4b0c846cdd
John Holland <john.holland@nice.org.uk>
2020-07-20 13:15:05 +0100

    Changed to line comments
    Not sure if this makes a difference, but was failing to build before...!? :anguished:

--- Comments/Startup.cs
+++ Comments/Startup.cs
@@ -183,18 +183,16 @@ namespace Comments
                     var reqPath = context.Request.Path;
                     if (reqPath.HasValue && reqPath.Value.Contains("."))
                     {
-						/*
-						Map static files paths to the root, for use within the 
-						if (reqPath.Value.Contains("/consultations"))
-						{
-							context.Request.Path = reqPath.Value.Replace("/consultations", "");
-						}
-						else if (reqPath.Value.IndexOf("favicon.ico", StringComparison.OrdinalIgnoreCase) == -1 && reqPath.Value.IndexOf("hot-update", StringComparison.OrdinalIgnoreCase) == -1)
-						{
-							context.Response.StatusCode = 404;
-							throw new FileNotFoundException($"Path {reqPath.Value} could not be found. Did you mean to load '/consultations{context.Request.Path.Value  }' instead?");
-						}
-						*/
+						// Map static files paths to the root, for use within the 
+						//if (reqPath.Value.Contains("/consultations"))
+						//{
+						//	context.Request.Path = reqPath.Value.Replace("/consultations", "");
+						//}
+						//else if (reqPath.Value.IndexOf("favicon.ico", StringComparison.OrdinalIgnoreCase) == -1 && reqPath.Value.IndexOf("hot-update", StringComparison.OrdinalIgnoreCase) == -1)
+						//{
+						//	context.Response.StatusCode = 404;
+						//	throw new FileNotFoundException($"Path {reqPath.Value} could not be found. Did you mean to load '/consultations{context.Request.Path.Value  }' instead?");
+						//}
 					}
 
 					return next();
