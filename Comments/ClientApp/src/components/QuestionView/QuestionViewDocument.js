ba53dc7665cb2316645226bf324fb9baefffaa8c
John Holland <john.holland@nice.org.uk>
2020-07-20 17:26:08 +0100

    Commented out pdfFonts bit for pdfMake
    Doesn't like webpack

--- Comments/ClientApp/src/components/QuestionView/QuestionViewDocument.js
+++ Comments/ClientApp/src/components/QuestionView/QuestionViewDocument.js
@@ -1,14 +1,14 @@
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+//import pdfFonts from "pdfmake/build/vfs_fonts"; //webpack has an issue with this
 import niceLogoBase64 from './nice-logo.png';
 
 var moment = require('moment');
 
 export const createQuestionPdf = (questionsForPDF, titleForPDF, endDate) => {
-	pdfMake.vfs = pdfFonts.pdfMake.vfs;
+	// pdfMake.vfs = pdfFonts.pdfMake.vfs;
 	const documentDefinition = createDocumentDefinition(questionsForPDF, titleForPDF, endDate);
 	pdfMake.createPdf(documentDefinition).open();
-}
+};
 
 export const createDocumentDefinition = (questionsForPDF, titleForPDF, endDate) => {
 
@@ -54,4 +54,4 @@ export const createDocumentDefinition = (questionsForPDF, titleForPDF, endDate) 
                 fontSize: 12
             }
     };
-}
+};
