58db4eff2bf0f396704778813b00f3be5594a0d9
John Holland <john.holland@nice.org.uk>
2020-09-18 12:10:51 +0100

    EC-734 Fixed focus issues for rhs drawer

--- Comments/ClientApp/src/helpers/accessibility-helpers.js
+++ Comments/ClientApp/src/helpers/accessibility-helpers.js
@@ -2,10 +2,11 @@
  * Pull focus to an element, principally for accessibility
  * @param {string} selection - The CSS-style query selector
  * @param {boolean} scroll - Whether or not to force the element to scroll into view
+ * @param {string} fallback - fallback query selector for another element should the selection above be unavailable
  */
-export const pullFocusByQuerySelector = (selection: string, scroll: boolean) => {
+export const pullFocusByQuerySelector = (selection: string, scroll: boolean, fallback: string) => {
 	setTimeout(() => {
-		const el = document.querySelector(selection);
+		const el = document.querySelector(selection) || document.querySelector(fallback);
 		if (el) {
 			if (el.getAttribute("tabindex") === null) {
 				el.setAttribute("tabindex", "-1");
