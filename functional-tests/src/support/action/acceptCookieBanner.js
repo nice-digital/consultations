b3feedd962495c531b3f53cdb680a69f1e07fa0e
Cheguegallagher <martin.gallagher@nice.org.uk>
2020-10-02 15:49:24 +0100

    adding in waits to see if they help

--- functional-tests/src/support/action/acceptCookieBanner.js
+++ functional-tests/src/support/action/acceptCookieBanner.js
@@ -2,7 +2,7 @@ export const acceptCookieBanner = () => {
 	var cookieBannerShown = browser.isVisible("body #ccc-content");
 	if (cookieBannerShown) {
 		browser.click("body #ccc-close");
-		browser.pause(1000);
+		browser.pause(2000);
 	} else {
 		return;
 	}
