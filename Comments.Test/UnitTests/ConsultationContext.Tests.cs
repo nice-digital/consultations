6857205cdb4b6e69b905163529f5e5e1012e2fa7
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-10-28 15:17:48 +0000

    EC-804 Remove redundant tests

--- Comments.Test/UnitTests/ConsultationContext.Tests.cs
+++ Comments.Test/UnitTests/ConsultationContext.Tests.cs
@@ -17,57 +17,6 @@ namespace Comments.Test.UnitTests
     public class ConsultationContext : TestBase
     {
         [Fact]
-        public void Comments_IsDeleted_Flag_is_not_Filtering_in_the_context()
-        {
-            // Arrange
-            ResetDatabase();
-            var sourceURI = "consultations://./consultation/1/document/1/chapter/introduction";
-            var commentText = Guid.NewGuid().ToString();
-            var createdByUserId = Guid.NewGuid().ToString();
-
-            var locationId = AddLocation(sourceURI);
-            AddComment(locationId, commentText, createdByUserId);
-
-            // Act
-            using (var consultationsContext = new ConsultationsContext(_options, _fakeUserService, _fakeEncryption))
-            {
-                var unfilteredLocations = consultationsContext.Location.Where(l =>
-                        l.SourceURI.Equals(sourceURI))
-                    .Include(l => l.Comment)
-                    .IgnoreQueryFilters()
-                    .ToList();
-
-                //Assert
-                unfilteredLocations.First().Comment.Count.ShouldBe(1);
-            }
-        }
-
-        [Fact]
-        public void Comments_IsDeleted_Flag_is_Filtering_in_the_context()
-        {
-            // Arrange
-            ResetDatabase();
-            var sourceURI = "consultations://./consultation/1/document/1/chapter/introduction";
-            var commentText = Guid.NewGuid().ToString();
-            var createdByUserId = Guid.NewGuid().ToString();
-
-            var locationId = AddLocation(sourceURI);
-            AddComment(locationId, commentText, createdByUserId);
-
-            // Act
-            using (var consultationsContext = new ConsultationsContext(_options, _fakeUserService, _fakeEncryption))
-            {
-                var filteredLocations = consultationsContext.Location.Where(l => l.SourceURI.Equals(sourceURI))
-                    .Include(l => l.Comment)
-                    .ToList();
-
-                //Assert
-                //removed while filtering is commented out.
-                filteredLocations.Single().Comment.Count.ShouldBe(0);
-            }
-        }
-
-	    [Fact]
 	    public void Get_All_Consultation_Comments_When_IsReview_Is_True()
 	    {
 		    // Arrange
