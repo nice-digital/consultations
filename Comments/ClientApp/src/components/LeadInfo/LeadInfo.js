c0d2b35e176e76a9ce8fd935b9b9ae51530169c6
Sue Garner <Sue.Garner@nice.org.uk>
2020-12-01 13:58:44 +0000

    EC-853 Added missing reference

--- Comments/ClientApp/src/components/LeadInfo/LeadInfo.js
+++ Comments/ClientApp/src/components/LeadInfo/LeadInfo.js
@@ -1,5 +1,6 @@
 ﻿import React, { Fragment } from "react";
 import { withRouter } from "react-router-dom";
+import Helmet from "react-helmet";
 import { withHistory } from "../HistoryContext/HistoryContext";
 import { Breadcrumbs } from "../Breadcrumbs/Breadcrumbs";
 
