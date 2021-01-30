7e0b436be01715989d6107187e44e409ce5edcd4
John Williams <jcwilliams@gmail.com>
2021-01-30 23:30:19 +0000

    EC-915 - fixing broken tests caused by null check on the identity, which can now be null, since the middleware.

--- Comments/ViewModels/LayoutBaseModel.cs
+++ Comments/ViewModels/LayoutBaseModel.cs
@@ -8,7 +8,7 @@ namespace Comments.ViewModels
 		public LayoutBaseModel(IPrincipal user, string signInURL, string signOutURL)
 		{
 			IsAuthenticated = user != null && user.Identity != null && user.Identity.IsAuthenticated;
-			DisplayName = user?.Identity.Name ?? "";
+			DisplayName = user?.Identity?.Name ?? "";
 
 			AccountsEnvironment = AppSettings.Environment.AccountsEnvironment.ToLower();
 
