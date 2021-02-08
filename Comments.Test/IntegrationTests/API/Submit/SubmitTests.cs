cbefe8abb3a0a5fd97773a8b24e3b7dbb54b6629
John Williams <jcwilliams@gmail.com>
2021-02-08 16:57:56 +0000

    EC-350 - test refactor to use testbaselight.

--- Comments.Test/IntegrationTests/API/Submit/SubmitTests.cs
+++ Comments.Test/IntegrationTests/API/Submit/SubmitTests.cs
@@ -15,11 +15,11 @@ using Xunit;
 
 namespace Comments.Test.IntegrationTests.API.Submit
 {
-    public class SubmitTests: TestBase
-    {
-	    public SubmitTests() : base(useRealSubmitService:true) {}
+	public class SubmitTests : TestBase
+	{
+		public SubmitTests() : base(useRealSubmitService: true) {}
 
-	    [Fact]
+		[Fact]
 		public async Task Submit_Comment_And_Answers()
 		{
 			//Arrange
@@ -31,13 +31,13 @@ namespace Comments.Test.IntegrationTests.API.Submit
 			var userId = Guid.Empty.ToString();
 			var locationId = AddLocation(sourceURI, _context);
 
-			var commentId = AddComment(locationId, commentText, userId, (int)StatusName.Draft, _context);
+			var commentId = AddComment(locationId, commentText, userId, (int) StatusName.Draft, _context);
 			var questionTypeId = 99;
 			var questionId = AddQuestion(locationId, questionTypeId, "Question Label");
 			var answerId = AddAnswer(questionId, userId, "Answer Label");
 
 			var userService = FakeUserService.Get(isAuthenticated: true, displayName: "Benjamin Button", userId: userId);
-			//var submitService = new SubmitService(_context, userService, _consultationService);
+
 			var commentService = new CommentService(_context, userService, _consultationService, _fakeHttpContextAccessor);
 
 			var commentsAndQuestions = commentService.GetCommentsAndQuestions(sourceURI, _urlHelper);
@@ -53,41 +53,54 @@ namespace Comments.Test.IntegrationTests.API.Submit
 			var deserialisedCommentsAndAnswers = JsonConvert.DeserializeObject<ViewModels.Submission>(responseString);
 
 			//Assert
-			comment.comment.StatusId.ShouldBe((int)StatusName.Submitted);
+			comment.comment.StatusId.ShouldBe((int) StatusName.Submitted);
 			deserialisedCommentsAndAnswers.Comments.First().Status.Name.ShouldBe(StatusName.Submitted.ToString());
-			deserialisedCommentsAndAnswers.Comments.First().Status.StatusId.ShouldBe((int)StatusName.Submitted);
+			deserialisedCommentsAndAnswers.Comments.First().Status.StatusId.ShouldBe((int) StatusName.Submitted);
 			deserialisedCommentsAndAnswers.Answers.First().Status.Name.ShouldBe(StatusName.Submitted.ToString());
-			deserialisedCommentsAndAnswers.Answers.First().Status.StatusId.ShouldBe((int)StatusName.Submitted);
+			deserialisedCommentsAndAnswers.Answers.First().Status.StatusId.ShouldBe((int) StatusName.Submitted);
 		}
+	}
+
+	public class SubmitToLeadTests : TestBaseLight
+	{
 
 		[Fact]
 		public async Task Submit_Comments_To_Organisation_Lead()
 		{
 			//Arrange
-			ResetDatabase();
-			_context.Database.EnsureCreated();
+			var organisationUserId = 1;
+			var organisationId = 1;
+			var fakeEncryption = new FakeEncryption();
+			var context = new ConsultationsContext(GetContextOptions(),
+				FakeUserService.Get(isAuthenticated: false, displayName: "Carl Spackler", userId: "Carl", testUserType: TestUserType.NotAuthenticated, organisationIdUserIsLeadOf: organisationId, organisationUserId: organisationUserId), fakeEncryption);
+			context.Database.EnsureDeleted();
 
 			var sourceURI = "consultations://./consultation/1/document/1/chapter/introduction";
 			var consultationId = 1;
-			var organisationId = 1;
+			
 			var authorisationSession = Guid.NewGuid();
 			var commentText = "Comment text";
 			var emailAddress = "me@test.com";
 
-			var organisationAuthorisationId = TestBaseDBHelpers.AddOrganisationAuthorisationWithLocation(organisationId, consultationId, _context);
-			var organisationUserId = TestBaseDBHelpers.AddOrganisationUser(_context, organisationAuthorisationId, authorisationSession, null);
+			var organisationAuthorisationId = TestBaseDBHelpers.AddOrganisationAuthorisationWithLocation(organisationId, consultationId, context);
+			TestBaseDBHelpers.AddOrganisationUser(context, organisationAuthorisationId, authorisationSession, null, organisationUserId: organisationUserId);
+			TestBaseDBHelpers.AddStatus(context, nameof(StatusName.Submitted), (int)StatusName.Submitted);
 
 			var userService = FakeUserService.Get(true, "Benjamin Button", null, TestUserType.NotAuthenticated, false, organisationUserId);
-			var consultationContext = new ConsultationsContext(_options, userService, _fakeEncryption);
-			var commentService = new CommentService(consultationContext, userService, _consultationService, _fakeHttpContextAccessor);
+			
+			var fakeHttpContextAccessor = FakeHttpContextAccessor.Get(isAuthenticated: false, testUserType: TestUserType.NotAuthenticated, organisationUserId: organisationUserId);
+			var fakeConsultationService = new FakeConsultationService();
+			var commentService = new CommentService(context, userService, fakeConsultationService, fakeHttpContextAccessor);
 
-			var locationId = AddLocation(sourceURI, _context);
-			var commentId = AddComment(locationId, commentText, null, (int)StatusName.Draft, _context, organisationUserId, null, organisationId);
+			var locationId = TestBaseDBHelpers.AddLocation(context, sourceURI);
+			var commentId = TestBaseDBHelpers.AddComment(context, locationId, commentText, null, (int)StatusName.Draft, organisationUserId, null, organisationId);
 
-			var commentsAndQuestions = commentService.GetCommentsAndQuestions(sourceURI, _urlHelper);
+			var commentsAndQuestions = commentService.GetCommentsAndQuestions(sourceURI, new FakeUrlHelper());
 
 			var submissionToLead = new SubmissionToLead(commentsAndQuestions.Comments, new List<ViewModels.Answer>(), emailAddress);
 			var content = new StringContent(JsonConvert.SerializeObject(submissionToLead), Encoding.UTF8, "application/json");
+
+			var (_server, _client) = InitialiseServerAndClient(context, userService, fakeConsultationService);
 
 			//Act
 			var response = await _client.PostAsync($"consultations/api/SubmitToLead", content);
