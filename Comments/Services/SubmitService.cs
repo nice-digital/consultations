5d49d3e1726270a2968a1c084eab7f8de5a2c6c7
Sue Garner <Sue.Garner@nice.org.uk>
2021-02-17 16:11:33 +0000

    EC-350 Refactored duplicate code into GetDuration()

--- Comments/Services/SubmitService.cs
+++ Comments/Services/SubmitService.cs
@@ -54,14 +54,7 @@ namespace Comments.Services
 			UpdateCommentsModel(submission.Comments, submissionToSave, submittedStatus);
 			UpdateAnswersModel(submission.Answers, submissionToSave, submittedStatus);
 
-			//now for analytics calculate the number of seconds between the user's first comment or answer and the submission date
-			var earliestDate = submissionToSave.SubmissionComment.Any() ? submissionToSave.SubmissionComment.Min(sc => sc.Comment.CreatedDate) : DateTime.MaxValue;
-			var earliestAnswer = submissionToSave.SubmissionAnswer.Any() ? submissionToSave.SubmissionAnswer.Min(sa => sa.Answer.CreatedDate) : DateTime.MaxValue;
-			if (earliestAnswer < earliestDate)
-			{
-				earliestDate = earliestAnswer;
-			}
-			submission.DurationBetweenFirstCommentOrAnswerSavedAndSubmissionInSeconds = (submissionToSave.SubmissionDateTime - earliestDate).TotalSeconds;
+			submission.DurationBetweenFirstCommentOrAnswerSavedAndSubmissionInSeconds = GetDuration(submissionToSave);
 
 			return (rowsUpdated: _context.SaveChanges(), validate: null);
 		}
@@ -105,14 +98,7 @@ namespace Comments.Services
 				_context.DuplicateAnswer(answerIds);
 			}
 
-			//now for analytics calculate the number of seconds between the user's first comment or answer and the submission date
-			var earliestDate = submissionToSave.SubmissionComment.Any() ? submissionToSave.SubmissionComment.Min(sc => sc.Comment.CreatedDate) : DateTime.MaxValue;
-			var earliestAnswer = submissionToSave.SubmissionAnswer.Any() ? submissionToSave.SubmissionAnswer.Min(sa => sa.Answer.CreatedDate) : DateTime.MaxValue;
-			if (earliestAnswer < earliestDate)
-			{
-				earliestDate = earliestAnswer;
-			}
-			submission.DurationBetweenFirstCommentOrAnswerSavedAndSubmissionInSeconds = (submissionToSave.SubmissionDateTime - earliestDate).TotalSeconds;
+			submission.DurationBetweenFirstCommentOrAnswerSavedAndSubmissionInSeconds = GetDuration(submissionToSave);
 
 			return (rowsUpdated: _context.SaveChanges(), validate: null, _context);
 		}
@@ -139,6 +125,18 @@ namespace Comments.Services
 				answerInViewModel.UpdateStatusFromDBModel(status);
 			}
 			_context.AddSubmissionAnswers(answerIds, submission.SubmissionId);
-		}	
+		}
+
+		private double GetDuration(Models.Submission submissionToSave)
+		{
+			//now for analytics calculate the number of seconds between the user's first comment or answer and the submission date
+			var earliestDate = submissionToSave.SubmissionComment.Any() ? submissionToSave.SubmissionComment.Min(sc => sc.Comment.CreatedDate) : DateTime.MaxValue;
+			var earliestAnswer = submissionToSave.SubmissionAnswer.Any() ? submissionToSave.SubmissionAnswer.Min(sa => sa.Answer.CreatedDate) : DateTime.MaxValue;
+			if (earliestAnswer < earliestDate)
+			{
+				earliestDate = earliestAnswer;
+			}
+			return (submissionToSave.SubmissionDateTime - earliestDate).TotalSeconds;
+		}
 	}
 }
