a3306f72e5629615c49d5e36fc47f2e4628c15d4
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-03-04 17:37:58 +0000

    EC-895 Fix tests

--- Comments.Test/Infrastructure/FakeConsultationService.cs
+++ Comments.Test/Infrastructure/FakeConsultationService.cs
@@ -42,12 +42,12 @@ namespace Comments.Test.Infrastructure
 	    public ConsultationState GetConsultationState(string sourceURI, PreviewState previewState, IEnumerable<Location> locations = null,
 		    ConsultationBase consultation = null)
 	    {
-			return new ConsultationState(DateTime.MinValue, _consultationIsOpen ? _consultationEndDate.Value : DateTime.MinValue, true, true, true, null, null);
+			return new ConsultationState(DateTime.MinValue, _consultationIsOpen ? _consultationEndDate.Value : DateTime.MinValue, true, true, true, null, null, false);
 		}
 	    public ConsultationState GetConsultationState(int consultationId, int? documentId, string reference, PreviewState previewState, IEnumerable<Location> locations = null,
 		    ConsultationBase consultation = null)
 	    {
-		    return new ConsultationState(DateTime.MinValue, _consultationIsOpen ? DateTime.MaxValue : DateTime.MinValue, true, true, true, null, null);
+		    return new ConsultationState(DateTime.MinValue, _consultationIsOpen ? DateTime.MaxValue : DateTime.MinValue, true, true, true, null, null, false);
 	    }
 
 	    public IEnumerable<Consultation> GetConsultations()
