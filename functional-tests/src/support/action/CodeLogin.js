df82436b17df988df566f1007ff191e074ce319f
FunmiOsho <60607637+FunmiOsho@users.noreply.github.com>
2021-05-19 19:46:49 +0100

    update to code generate and login js

--- functional-tests/src/support/action/CodeLogin.js
+++ functional-tests/src/support/action/CodeLogin.js
@@ -1,22 +1,15 @@
-import acceptCookieBanner from "./acceptCookieBanner";
 import waitForVisible from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitForVisible";
+import pause from	"@nice-digital/wdio-cucumber-steps/lib/support/action/pause";
+import setInputField from "@nice-digital/wdio-cucumber-steps/lib/support/action/setInputField";
 
 export const CodeLogin = () => {
 
-	browser.refresh();
-	browser.pause(2000);
-	browser.waitForVisible("[for='respondingAsOrg--organisation']");
-	browser.click("[for='respondingAsOrg--organisation']");
-	browser.pause(2000);
-	browser.waitForVisible("[for='respondingAsOrgType--code']");
-	browser.click("[for='respondingAsOrgType--code']");
-	browser.waitForVisible("[data-qa-sel='OrganisationCodeLogin']");
-	browser.pause(5000);
-	browser.setValue('input#orgCode-undefined', ['Control', 'V'])
-	browser.pause(5000);
+	browser.click("[data-qa-sel='loginpanel-label-organisation']", 2000);
+	browser.click("[data-qa-sel='loginpanel-label-code']");
+	waitForVisible("[data-qa-sel='OrganisationCodeLogin']");
+	setInputField("setValue",  ['Control', 'V'], "[data-qa-sel='OrganisationCodeLogin']");
+  pause(2000);
 	browser.click(".btn--cta");
-	browser.pause(2000);
-	waitForVisible("[data-qa-sel='open-commenting-panel']");
-  browser.click("[data-qa-sel='open-commenting-panel']");
+	browser.click("[data-qa-sel='open-commenting-panel']");
 };
 export default CodeLogin;
