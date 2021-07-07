0c53219cbd4d0d9764de7bde6d5f1387648cc8e8
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-07-07 11:16:45 +0100

    EC-988 Returned json in the right shape

--- Indev.MockApi/server.js
+++ Indev.MockApi/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const jsonServer = require('json-server');
 const http = require('http');
+const url = require('url');
 
 const http_port = process.env.HTTP_PORT || 3001;
 
@@ -13,6 +14,17 @@ const rewriter = jsonServer.rewriter({
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
 const router = jsonServer.router('db.json');
+
+router.render = (req, res) => {
+	let requestUrl = url.parse(req.url);
+	if (requestUrl.path.includes('list')){
+		res.jsonp({
+			_embedded: res.locals.data
+		  })
+	}else{
+		res.jsonp(res.locals.data[0]);
+	}
+  }
 
 server.use(rewriter);
 server.use(middleware);
