8cced05b092af99c25a91a336521207d6e2a0229
Funmi Osho <oluwafunmilayo.osho@nice.org.uk>
2022-02-08 18:01:08 +0000

    Update sign-in header element with new class id

--- functional-tests/src/support/action/idamGlobalnavLoginAdmin.js
+++ functional-tests/src/support/action/idamGlobalnavLoginAdmin.js
@@ -6,16 +6,16 @@ export const idamGlobalNavLoginAdmin = (username, password) => {
 		return;
 	}
 
-	var headerMenuExists = browser.waitForExist("body .gn_qKxI2", 2000);
+	var headerMenuExists = browser.waitForExist("body .gn_QaU-b", 2000);
 	if (headerMenuExists !== true) {
 		browser.refresh();
 	} else {
 		var headerMenuShown = browser.isVisible("body #header-menu-button");
 		if (headerMenuShown) {
 			browser.click("body #header-menu-button");
-			browser.click("body .gn_qKxI2");
+			browser.click("body .gn_QaU-b");
 		} else {
-			browser.click("body .gn_qKxI2");
+			browser.click("body .gn_QaU-b");
 		}
 	}
 	idamLogin(username, password);
