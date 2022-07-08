2a5861a6b2035e66f0ac9401afc85178b7e7c2b3
Cheguegallagher <martin.gallagher@nice.org.uk>
2022-07-08 11:18:09 +0100

    Output error shots for Accessibility

--- functional-tests/wdio.accessibility.conf.ts
+++ functional-tests/wdio.accessibility.conf.ts
@@ -35,16 +35,15 @@ export const config: WebdriverIO.Config = {
 	baseUrl: "https://niceorg/consultations/",
 	reporters: [
 		"spec",
-		isTeamCity && "teamcity",
-		isInDocker && [
-			"allure",
+		"teamcity",
+		["allure",
 			{
 				useCucumberStepReporter: true,
 				// Turn on screenshot reporting for error shots
 				disableWebdriverScreenshotsReporting: false,
 			},
-		],
-	].filter(Boolean) as WebdriverIO.Config["reporters"],
+		]
+	],
 
 	framework: "cucumber",
 	cucumberOpts: {
@@ -56,9 +55,11 @@ export const config: WebdriverIO.Config = {
 		timeout: 1500000,
 	},
 
-	afterStep: async function (_test, _scenario, { error }) {
+	afterStep: async function (_test, _scenario, { error, passed }) {
 		// Take screenshots on error, these end up in the Allure reports
+		var fileName = "errorShots/" + "ERROR_" + _scenario.name + ".png";
 		if (error) await browser.takeScreenshot();
+		if (error) await browser.saveScreenshot(fileName);
 	},
 
 	autoCompileOpts: {
