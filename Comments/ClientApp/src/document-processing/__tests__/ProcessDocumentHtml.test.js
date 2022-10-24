933bf23e7cad3c209eb028328b1728a13d583d02
John Holland <john.holland@nice.org.uk>
2022-10-24 10:57:23 +0100

    EC-1026 Changed tests from enzyme to rtl - batch 5

--- Comments/ClientApp/src/document-processing/__tests__/ProcessDocumentHtml.test.js
+++ Comments/ClientApp/src/document-processing/__tests__/ProcessDocumentHtml.test.js
@@ -1,114 +1,138 @@
-/* eslint-env jest */
-
-import { ProcessDocumentHtml } from "../ProcessDocumentHtml";
 import React from "react";
-import { mount } from "enzyme";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ProcessDocumentHtml } from "../ProcessDocumentHtml";
 
-describe("[ClientApp]", () => {
+test("renders a button if the html contains an anchor with a type of 'section'", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	expect(screen.getByRole("button", { name: "Comment on section: Foo" })).toBeInTheDocument();
+});
 
-	describe("Render Document HTML", () => {
+test("renders a button if the html contains an anchor with a type of 'chapter'", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test' data-heading-type='chapter'>Bar</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	expect(screen.getByRole("button", { name: "Comment on chapter: Bar" })).toBeInTheDocument();
+});
 
-		function setupHtml(allowComments, html) {
-			const url = "/1/1/guidance";
-			const clickFunction = jest.fn();
-			return {
-				wrapper:
-					mount(
-						<div>
-							<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={allowComments}/>
-						</div>,
-					),
-				clickFunction,
-			};
-		}
+test("doesn't render a button if there is no anchor with a type", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test'>Bar</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	expect(screen.queryAllByRole("button").length).toEqual(0);
+});
 
-		it("renders a button if the html contains an anchor with a type of 'section'", () => {
-			const instance = setupHtml(true,
-				"<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>",
-			);
-			expect(instance.wrapper.find("button").text()).toEqual("Comment on section: Foo");
-		});
+test("renders a button if the html contains a numbered paragraph (subsection)", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<p class='numbered-paragraph annotator-numbered-paragraph' id='np-1-3-1' data-heading-type='numbered-paragraph' title='Comment on numbered paragraph'>" +
+	"        <span class='paragraph-number'>1.3.1 </span>Healthcare providers should:</p>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	expect(screen.getByRole("button", { name: "Comment on subsection: 1.3.1" })).toBeInTheDocument();
+});
 
-		it("renders a button if the html contains an anchor with a type of 'chapter'", () => {
-			const instance = setupHtml(true,
-				"<div><a id='bar' href='#test' data-heading-type='chapter'>Bar</a></div>",
-			);
-			expect(instance.wrapper.find("button").text()).toEqual("Comment on chapter: Bar");
-		});
-
-		it("doesn't render a button if there is no anchor with a type", () => {
-			const instance = setupHtml(true,
-				"<div><a id='bar' href='#test'>Bar</a></div>",
-			);
-			expect(instance.wrapper.find("button").length).toEqual(0);
-		});
-
-		it("renders a button if the html contains a numbered paragraph (subsection)", () => {
-			const instance = setupHtml(true,
-				"<p class='numbered-paragraph annotator-numbered-paragraph' id='np-1-3-1' data-heading-type='numbered-paragraph' title='Comment on numbered paragraph'>" +
-				"        <span class='paragraph-number'>1.3.1 </span>Healthcare providers should:</p>",
-			);
-			expect(instance.wrapper.find("button").text()).toEqual("Comment on subsection: 1.3.1 ");
-		});
-
-		it("button fires passed function with expected object for a chapter", () => {
-
-			const instance = setupHtml(true,
-				"<h2 class=\"title\">\n" +
-				"    <a id=\"recommendations\" style=\"position:relative\" class=\"annotator-chapter\" data-heading-type=\"chapter\" title=\"Comment on chapter\" xmlns=\"\">1.1 Recommendations<span class=\"annotator-adder\" /></a>\n" +
-				"  </h2>",
-			);
-			instance.wrapper.find("button").simulate("click");
-			expect(instance.clickFunction).toHaveBeenCalledWith(expect.anything(), {
-				sourceURI: "/1/1/guidance",
-				commentText: "",
-				commentOn: "chapter",
-				htmlElementID: "",
-				sectionNumber: "1.1",
-				quote: "1.1 Recommendations",
-			});
-		});
-
-		it("button fires passed function with expected object for a section", () => {
-			const instance = setupHtml(true,	"<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>",
-			);
-			instance.wrapper.find("button").simulate("click");
-			expect(instance.clickFunction).toHaveBeenCalledWith(expect.anything(), {
-				sourceURI: "/1/1/guidance",
-				commentText: "",
-				commentOn: "section",
-				htmlElementID: "bar",
-				sectionNumber: undefined,
-				quote: "Foo",
-			});
-		});
-
-		it("button fires passed function with expected object for a subsection", () => {
-			const instance = setupHtml(true, "<p class='numbered-paragraph annotator-numbered-paragraph' id='np-1-3-1' data-heading-type='numbered-paragraph' title='Comment on numbered paragraph'><span class='paragraph-number'>1.3.1 </span>Healthcare providers should:</p>");
-			instance.wrapper.find("button").simulate("click");
-			expect(instance.clickFunction).toHaveBeenCalledWith(expect.anything(), {
-				sourceURI: "/1/1/guidance",
-				commentText: "",
-				commentOn: "subsection",
-				htmlElementID: "np-1-3-1",
-				sectionNumber: "1.3.1",
-				quote: "1.3.1 ",
-			});
-		});
-
-		it("does not render a button when allowComments is false, even if the html contains an anchor with a type of 'section'", () => {
-			const instance = setupHtml(false,
-				"<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>",
-			);
-			expect(instance.wrapper.find("button").length).toEqual(0);
-		});
-
-		it("does not render a button when allowComments is false, even if the html contains an anchor with a type of 'chapter'", () => {
-			const instance = setupHtml(false,
-				"<div><a id='bar' href='#test' data-heading-type='chapter'>Bar</a></div>",
-			);
-			expect(instance.wrapper.find("button").length).toEqual(0);
-		});
-		
+test("button fires passed function with expected object for a chapter", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<h2 class=\"title\">\n" +
+	"    <a id=\"recommendations\" style=\"position:relative\" class=\"annotator-chapter\" data-heading-type=\"chapter\" title=\"Comment on chapter\" xmlns=\"\">1.1 Recommendations<span class=\"annotator-adder\" /></a>\n" +
+	"  </h2>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	fireEvent.click(screen.getByRole("button"));
+	expect(clickFunction).toHaveBeenCalledWith(expect.anything(), {
+		sourceURI: "/1/1/guidance",
+		commentText: "",
+		commentOn: "chapter",
+		htmlElementID: "",
+		sectionNumber: "1.1",
+		quote: "1.1 Recommendations",
 	});
+});
+
+test("button fires passed function with expected object for a section", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	fireEvent.click(screen.getByRole("button"));
+	expect(clickFunction).toHaveBeenCalledWith(expect.anything(), {
+		sourceURI: "/1/1/guidance",
+		commentText: "",
+		commentOn: "section",
+		htmlElementID: "bar",
+		sectionNumber: undefined,
+		quote: "Foo",
+	});
+});
+
+test("button fires passed function with expected object for a subsection", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<p class='numbered-paragraph annotator-numbered-paragraph' id='np-1-3-1' data-heading-type='numbered-paragraph' title='Comment on numbered paragraph'><span class='paragraph-number'>1.3.1 </span>Healthcare providers should:</p>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={true}/>
+		</div>,
+	);
+	fireEvent.click(screen.getByRole("button"));
+	expect(clickFunction).toHaveBeenCalledWith(expect.anything(), {
+		sourceURI: "/1/1/guidance",
+		commentText: "",
+		commentOn: "subsection",
+		htmlElementID: "np-1-3-1",
+		sectionNumber: "1.3.1",
+		quote: "1.3.1 ",
+	});
+});
+
+test("does not render a button when allowComments is false, even if the html contains an anchor with a type of 'section'", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test' data-heading-type='section'>Foo</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={false}/>
+		</div>,
+	);
+	expect(screen.queryAllByRole("button").length).toEqual(0);
+});
+
+test("does not render a button when allowComments is false, even if the html contains an anchor with a type of 'chapter'", () => {
+	const url = "/1/1/guidance";
+	const clickFunction = jest.fn();
+	const html = "<div><a id='bar' href='#test' data-heading-type='chapter'>Bar</a></div>";
+	render(
+		<div>
+			<ProcessDocumentHtml content={html} onNewCommentClick={clickFunction} url={url} allowComments={false}/>
+		</div>,
+	);
+	expect(screen.queryAllByRole("button").length).toEqual(0);
 });
