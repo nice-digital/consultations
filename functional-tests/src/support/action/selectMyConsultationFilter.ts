080e4ab211fc83559d7101187d59fdacbb94c48e
FunmiOsho <60607637+FunmiOsho@users.noreply.github.com>
2023-05-15 11:45:37 +0100

    Update to feature & supporting files due to change

--- functional-tests/src/support/action/selectMyConsultationFilter.ts
+++ functional-tests/src/support/action/selectMyConsultationFilter.ts
@@ -1,14 +1,14 @@
-import {checkSelected} from "@nice-digital/wdio-cucumber-steps/lib/support/check/checkSelected";
+import {clickElement} from "@nice-digital/wdio-cucumber-steps/lib/support/action/clickElement";
 import {waitForDisplayed} from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitForDisplayed";
 import {pause} from "@nice-digital/wdio-cucumber-steps/lib/support/action/pause";
 import selectors from "../selectors";
 
-export async function validateResponsesFilterChecked(): Promise<void> {
+export async function selectMyConsultationFilter(): Promise<void> {
 
 	await waitForDisplayed(selectors.adminDownloadPage.myConsultationsFilter, "");
-	await checkSelected(selectors.adminDownloadPage.myConsultationsFilter, "true");
+	await clickElement("click", "button", selectors.adminDownloadPage.myConsultationsFilter);
 	await pause("1000");
 
 };
 
-export default validateResponsesFilterChecked;
+export default selectMyConsultationFilter;
