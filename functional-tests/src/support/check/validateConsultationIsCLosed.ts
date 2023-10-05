adf925288fe709696a17730dab5893f7ce8857dd
Cheguegallagher <martin.gallagher@nice.org.uk>
2023-10-05 17:21:44 +0100

    EC-1067 Using waitForExist directly from API to ensure flasecase works

--- functional-tests/src/support/check/validateConsultationIsCLosed.ts
+++ functional-tests/src/support/check/validateConsultationIsCLosed.ts
@@ -29,27 +29,32 @@ export async function validateConsultationClosedMessage(): Promise<void> {
 
 export async function validateCommentOnDocIsDisabled(): Promise<void> {
 	await waitForDisplayed(selectors.documentPage.documentContainer, "");
-	await isExisting(selectors.documentPage.commentOnDocButton, "false");
+	await $(selectors.documentPage.commentOnDocButton).waitForExist({reverse: true});
+	// await isExisting(selectors.documentPage.commentOnDocButton, "false");
 	await pause("1000");
 }
 
 export async function validateCommentOnChapterIsDisabled(): Promise<void> {
 	await waitForDisplayed(selectors.documentPage.documentContainer, "");
-	await isExisting(".chapter > .title [data-qa-sel='in-text-comment-button']", "false");
+	await $(".chapter > .title [data-qa-sel='in-text-comment-button']").waitForExist({reverse: true})
+	// await isExisting(".chapter > .title [data-qa-sel='in-text-comment-button']", "false");
 	await pause("1000");
 }
 
 export async function validateCommentOnSectionIsDisabled(): Promise<void> {
 	await waitForDisplayed(selectors.documentPage.documentContainer, "");
+	await pause("1000")
 	await clickElement("click", "selector", "[data-qa-sel='nav-list-item']:nth-of-type(2)");
 	await waitForDisplayed(selectors.documentPage.documentContainer, "");
-	await isExisting("[data-qa-sel='nav-list-item']:nth-of-type(2)", "false");
+	await $(".section:first-of-type > .title [data-qa-sel='in-text-comment-button']").waitForExist({reverse: true});
+	// await isExisting("[data-qa-sel='nav-list-item']:nth-of-type(2)", "false");
 	await pause("1000");
 }
 
 export async function validateCommentOnSubSectionIsDisabled(): Promise<void> {
 	await waitForDisplayed(selectors.documentPage.documentContainer, "");
-	await isExisting("[data-gtm-label='Comment on subsection']", "false");
+	await $("[data-gtm-label='Comment on subsection']").waitForExist({reverse: true});
+	// await isExisting("[data-gtm-label='Comment on subsection']", "false");
 	await pause("1000");
 }
 export default validateConsultationIsClosed;
