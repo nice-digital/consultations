17c916b3364c701d9b3a8be875ef20a0281004ca
imranazad <imran.azad@nnice.org.uk>
2026-03-31 20:53:46 +0100

    EC-1197 Fix Failing Tests

--- Comments.Test/Infrastructure/TestBase.cs
+++ Comments.Test/Infrastructure/TestBase.cs
@@ -172,9 +172,9 @@ namespace Comments.Test.Infrastructure
                 {
 	                config.AddInMemoryCollection(new Dictionary<string, string>
 	                {
-		                ["EncryptionConfig:Key"] = "Key",
-		                ["EncryptionConfig:IV"] = "IV"
-	                });
+		                ["Encryption:Key"] = "Key",
+		                ["Encryption:IV"] = "IV"
+});
 
 	                config.AddUserSecrets<Startup>(); // optional
                 }).ConfigureServices(services =>
