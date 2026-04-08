d32da7a0cb8e94b8586b1a7771d76420ab02bdeb
imranazad <imran.azad@nnice.org.uk>
2026-04-08 10:30:08 +0100

    EC-1107 Stop Redirect

--- Comments/Startup.cs
+++ Comments/Startup.cs
@@ -131,11 +131,11 @@ namespace Comments
 
             if (!Environment.IsDevelopment())
 	        {
-		        services.AddHttpsRedirection(options =>
+		        /*services.AddHttpsRedirection(options =>
 		        {
 			        options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
 			        options.HttpsPort = 443;
-		        });
+		        });*/
 	        }
 
 	        services.Configure<ForwardedHeadersOptions>(options =>
